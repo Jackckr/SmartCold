f6e4e520fab5322893320d994cab26f7357afb59
qiunian.sun <qiunian.sun@dianping.com>
2016-03-19 16:31:47 +0800

    update coldStorage and compressorGroup table

--- cold-web/src/main/java/com/smartcold/manage/cold/entity/ColdStorageEntity.java
+++ cold-web/src/main/java/com/smartcold/manage/cold/entity/ColdStorageEntity.java
@@ -4,163 +4,53 @@ import java.util.Date;
 
 public class ColdStorageEntity {
 
-	private int id;
+    private int id;
 
-	private int storageID;
+    private int storageID;
 
-	private float Temperature;
+    private float temperature;
 
-	private float defrostingTemperature;
+    private Date time;
 
-	private int refrigerationTime;
+    private Date addTime;
 
-	private int coldFan;
+    public int getId() {
+        return id;
+    }
 
-	private int refrigerationValve;
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	private int defrosting;
+    public int getStorageID() {
+        return storageID;
+    }
 
-	private int warmValve;
+    public void setStorageID(int storageID) {
+        this.storageID = storageID;
+    }
 
-	private int returnAirValue;
+    public float getTemperature() {
+        return temperature;
+    }
 
-	private int startSwitch;
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
 
-	private int doorSwitch;
+    public Date getTime() {
+        return time;
+    }
 
-	private int lightSwitch;
+    public void setTime(Date time) {
+        this.time = time;
+    }
 
-	private int overloadProtected;
+    public Date getAddTime() {
+        return addTime;
+    }
 
-	private int fanStatus;
-
-	private Date addTime;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getStorageID() {
-		return storageID;
-	}
-
-	public void setStorageID(int storageID) {
-		this.storageID = storageID;
-	}
-
-	public float getTemperature() {
-		return Temperature;
-	}
-
-	public void setTemperature(float temperature) {
-		Temperature = temperature;
-	}
-
-	public float getDefrostingTemperature() {
-		return defrostingTemperature;
-	}
-
-	public void setDefrostingTemperature(float defrostingTemperature) {
-		this.defrostingTemperature = defrostingTemperature;
-	}
-
-	public int getRefrigerationTime() {
-		return refrigerationTime;
-	}
-
-	public void setRefrigerationTime(int refrigerationTime) {
-		this.refrigerationTime = refrigerationTime;
-	}
-
-	public int getColdFan() {
-		return coldFan;
-	}
-
-	public void setColdFan(int coldFan) {
-		this.coldFan = coldFan;
-	}
-
-	public int getRefrigerationValve() {
-		return refrigerationValve;
-	}
-
-	public void setRefrigerationValve(int refrigerationValve) {
-		this.refrigerationValve = refrigerationValve;
-	}
-
-	public int getDefrosting() {
-		return defrosting;
-	}
-
-	public void setDefrosting(int defrosting) {
-		this.defrosting = defrosting;
-	}
-
-	public int getWarmValve() {
-		return warmValve;
-	}
-
-	public void setWarmValve(int warmValve) {
-		this.warmValve = warmValve;
-	}
-
-	public int getReturnAirValue() {
-		return returnAirValue;
-	}
-
-	public void setReturnAirValue(int returnAirValue) {
-		this.returnAirValue = returnAirValue;
-	}
-
-	public int getStartSwitch() {
-		return startSwitch;
-	}
-
-	public void setStartSwitch(int startSwitch) {
-		this.startSwitch = startSwitch;
-	}
-
-	public int getDoorSwitch() {
-		return doorSwitch;
-	}
-
-	public void setDoorSwitch(int doorSwitch) {
-		this.doorSwitch = doorSwitch;
-	}
-
-	public int getLightSwitch() {
-		return lightSwitch;
-	}
-
-	public void setLightSwitch(int lightSwitch) {
-		this.lightSwitch = lightSwitch;
-	}
-
-	public int getOverloadProtected() {
-		return overloadProtected;
-	}
-
-	public void setOverloadProtected(int overloadProtected) {
-		this.overloadProtected = overloadProtected;
-	}
-
-	public int getFanStatus() {
-		return fanStatus;
-	}
-
-	public void setFanStatus(int fanStatus) {
-		this.fanStatus = fanStatus;
-	}
-
-	public Date getAddTime() {
-		return addTime;
-	}
-
-	public void setAddTime(Date addTime) {
-		this.addTime = addTime;
-	}
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
 }
