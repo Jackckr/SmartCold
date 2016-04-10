5c00321d107b834c2bfa96c1d74f9046bfcd8506
keren.chen <keren.chen@dianping.com>
2016-04-10 13:38:24 +0800

    just commit

--- cold-web/src/main/java/com/smartcold/manage/cold/controller/UserController.java
+++ cold-web/src/main/java/com/smartcold/manage/cold/controller/UserController.java
@@ -1,24 +1,17 @@
 package com.smartcold.manage.cold.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.smartcold.manage.cold.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import com.smartcold.manage.cold.dao.UserMapper;
+import com.smartcold.manage.cold.entity.Role;
+import com.smartcold.manage.cold.entity.RoleUser;
+import com.smartcold.manage.cold.entity.UserEntity;
 import com.smartcold.manage.cold.service.CompanyRdcService;
 import com.smartcold.manage.cold.service.CompanyService;
 import com.smartcold.manage.cold.service.CompanyUserService;
@@ -30,118 +23,110 @@ import com.smartcold.manage.cold.service.UserService;
 @RequestMapping(value = "/user")
 public class UserController extends BaseController {
 
-    @Autowired
-    private UserMapper userDao;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private CompanyService companyService;
-    @Autowired
-    private CompanyUserService companyUserService;
-    @Autowired
-    private CompanyRdcService companyRdcService;
-    @Autowired
-    private RoleService roleService;
-    @Autowired
-    private RoleUserService roleUserService;
+	@Autowired
+	private UserMapper userDao;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private CompanyService companyService;
+	@Autowired
+	private CompanyUserService companyUserService;
+	@Autowired
+	private CompanyRdcService companyRdcService;
+	@Autowired
+	private RoleService roleService;
+	@Autowired
+	private RoleUserService roleUserService;
 
-    /**
-     * @param @param  userName
-     * @param @param  password
-     * @param @param  request
-     * @param @param  response
-     * @param @return ModelAndView
-     * @param @throws Exception
-     * @return true/false
-     * @throws
-     * @Title: login UserController
-     * @Description: 用户登录
-     */
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    @ResponseBody
-    public Object login(HttpServletRequest request, String userName, String password) {
-        //UserEntity user = userDao.findByPassword(userName, password);
-        UserEntity user = userService.getUserByNAndP(userName, password);
-        if (user != null) {
-            RoleUser roleUser = roleUserService.getRoleIdByUserId(user.getId());
-            Role role = roleService.getRoleByRoleId(roleUser.getRoleid());
-            user.setPassword("******");
-            user.setRole(role.getId());
-            request.getSession().setAttribute("user", user);
-            request.getSession().setAttribute("cookie", user);
-            return true;
-        }
-        return false;
-    }
-	/*@SuppressWarnings({ "finally", "rawtypes", "unchecked" })
-	@RequestMapping(value = "/login",method = RequestMethod.GET)
+	/**
+	 * @param @param
+	 *            userName
+	 * @param @param
+	 *            password
+	 * @param @param
+	 *            request
+	 * @param @param
+	 *            response
+	 * @param @return
+	 *            ModelAndView
+	 * @param @throws
+	 *            Exception
+	 * @return true/false
+	 * @throws @Title:
+	 *             login UserController
+	 * @Description: 用户登录
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	@ResponseBody
-	public ModelAndView login2(HttpServletRequest request,HttpServletResponse response,
-			String userName, String password)throws Exception {
-		ModelAndView mav = new ModelAndView();
-		MappingJackson2JsonView view = new MappingJackson2JsonView();
-		Map map = new HashMap();
-		try{
-			UserEntity user = userService.getUserByNAndP(userName, password);
-			if (user!=null) {
-		    RoleUser roleUser = roleUserService.getRoleIdByUserId(user.getId());
-		    Role role = roleService.getRoleByRoleId(roleUser.getRoleid());
-		    String roleName = role.getName();
-		    CompanyUser compUser = companyUserService.getComUserByUserId(user.getId());
-		    Company company = companyService.getCompByCompId(compUser.getCompanyid());
-		    String companyName = company.getName();
-		    ArrayList<Integer> rdcIdList = new ArrayList<Integer>();
-			if(compUser.getCompanyid()!=null)
-			{
-            	List<CompanyRdc> compRdcList = companyRdcService.getCompRdcsByCompId(compUser.getCompanyid());
-            	for(int i=0;i<compRdcList.size(); i++){
-            	         rdcIdList.add(compRdcList.get(i).getRdcid());
-            	}
-			}
-		    //取得角色对应的权限
-	*//*	    List<Privilege> privList = new ArrayList<Privilege>();
-		    List<PrivilegeRole> privRoleList = privilegeRoleService
-					                          .getPrivRoleByRoleId(roleUser.getRoleid());
-		    for (int i = 0; i < privRoleList.size(); i++) {
-			     Privilege priv = privilegeService.getPrivByPrivId(privRoleList.get(i).getId());
-				 privList.add(priv);
-							}*//*
+	public Object login(HttpServletRequest request, String userName, String password) {
+		// UserEntity user = userDao.findByPassword(userName, password);
+		UserEntity user = userService.getUserByNAndP(userName, password);
+		if (user.getId() != 0) {
+			RoleUser roleUser = roleUserService.getRoleIdByUserId(user.getId());
+			Role role = roleService.getRoleByRoleId(roleUser.getRoleid());
 			user.setPassword("******");
-			HttpSession session = request.getSession();
 			user.setRole(role.getId());
-			session.setAttribute("user", user);
-			session.setAttribute("cookie", user);			
-			map.put("companyName", companyName);
-			map.put("roleName", roleName);
-			map.put("rdcIdList", rdcIdList);
-			map.put("result", Boolean.TRUE);
-		}else {
-			map.put("result", Boolean.FALSE);
+			request.getSession().setAttribute("user", user);
+			request.getSession().setAttribute("cookie", user);
+			return true;
 		}
-		}catch(Exception e){
-			map.put("result", Boolean.FALSE);
-			e.printStackTrace();
-		}finally{
-			view.setAttributesMap(map);
-			mav.setView(view);
-			return mav;
+		return false;
+	}
+	/*
+	 * @SuppressWarnings({ "finally", "rawtypes", "unchecked" })
+	 * 
+	 * @RequestMapping(value = "/login",method = RequestMethod.GET)
+	 * 
+	 * @ResponseBody public ModelAndView login2(HttpServletRequest
+	 * request,HttpServletResponse response, String userName, String
+	 * password)throws Exception { ModelAndView mav = new ModelAndView();
+	 * MappingJackson2JsonView view = new MappingJackson2JsonView(); Map map =
+	 * new HashMap(); try{ UserEntity user =
+	 * userService.getUserByNAndP(userName, password); if (user!=null) {
+	 * RoleUser roleUser = roleUserService.getRoleIdByUserId(user.getId()); Role
+	 * role = roleService.getRoleByRoleId(roleUser.getRoleid()); String roleName
+	 * = role.getName(); CompanyUser compUser =
+	 * companyUserService.getComUserByUserId(user.getId()); Company company =
+	 * companyService.getCompByCompId(compUser.getCompanyid()); String
+	 * companyName = company.getName(); ArrayList<Integer> rdcIdList = new
+	 * ArrayList<Integer>(); if(compUser.getCompanyid()!=null) {
+	 * List<CompanyRdc> compRdcList =
+	 * companyRdcService.getCompRdcsByCompId(compUser.getCompanyid()); for(int
+	 * i=0;i<compRdcList.size(); i++){
+	 * rdcIdList.add(compRdcList.get(i).getRdcid()); } } //取得角色对应的权限
+	 *//*
+		 * List<Privilege> privList = new ArrayList<Privilege>();
+		 * List<PrivilegeRole> privRoleList = privilegeRoleService
+		 * .getPrivRoleByRoleId(roleUser.getRoleid()); for (int i = 0; i <
+		 * privRoleList.size(); i++) { Privilege priv =
+		 * privilegeService.getPrivByPrivId(privRoleList.get(i).getId());
+		 * privList.add(priv); }
+		 *//*
+		 * user.setPassword("******"); HttpSession session =
+		 * request.getSession(); user.setRole(role.getId());
+		 * session.setAttribute("user", user); session.setAttribute("cookie",
+		 * user); map.put("companyName", companyName); map.put("roleName",
+		 * roleName); map.put("rdcIdList", rdcIdList); map.put("result",
+		 * Boolean.TRUE); }else { map.put("result", Boolean.FALSE); }
+		 * }catch(Exception e){ map.put("result", Boolean.FALSE);
+		 * e.printStackTrace(); }finally{ view.setAttributesMap(map);
+		 * mav.setView(view); return mav; } }
+		 */
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	@ResponseBody
+	public Object logout(HttpServletRequest request) {
+		request.getSession().setAttribute("user", null);
+		return true;
+	}
+
+	@RequestMapping(value = "/findUser", method = RequestMethod.GET)
+	@ResponseBody
+	public Object findUser(HttpServletRequest request) {
+		UserEntity user = (UserEntity) request.getSession().getAttribute("user");
+		if (user == null) {
+			user = new UserEntity();
 		}
-  }*/
-
-    @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    @ResponseBody
-    public Object logout(HttpServletRequest request) {
-        request.getSession().setAttribute("user", null);
-        return true;
-    }
-
-    @RequestMapping(value = "/findUser", method = RequestMethod.GET)
-    @ResponseBody
-    public Object findUser(HttpServletRequest request) {
-        UserEntity user = (UserEntity) request.getSession().getAttribute("user");
-        if (user == null) {
-            user = new UserEntity();
-        }
-        return user;
-    }
+		return user;
+	}
 }
