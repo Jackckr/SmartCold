bb193476b21dcb0ae846cdf400f5ace3105c24d5
qiunian.sun <qiunian.sun@dianping.com>
2016-04-16 11:59:32 +0800

    add list and comment

---
+++ zigbee-web/src/main/webapp/app/js/coldStorageComment.js
@@ -0,0 +1,8 @@
+/**
+ * Created by qiunian.sun on 16/4/9.
+ */
+coldWeb.controller('coldStorageComment', function ($rootScope, $scope, $cookies, $http ,$location) {
+
+    alert("冷库评论耶");
+
+});
---
+++ zigbee-web/src/main/webapp/app/js/coldStoragelist.js
@@ -0,0 +1,8 @@
+/**
+ * Created by qiunian.sun on 16/4/9.
+ */
+coldWeb.controller('coldStoragelist', function ($rootScope, $scope, $cookies, $http ,$location) {
+
+    alert("冷库列表耶");
+
+});
