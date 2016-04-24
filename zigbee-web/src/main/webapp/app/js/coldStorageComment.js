7b29e1dd78cbf8bb2abe240edbd77e60aa26d77a
qiunian.sun <qiunian.sun@dianping.com>
2016-04-24 15:31:40 +0800

    fix

--- zigbee-web/src/main/webapp/app/js/coldStorageComment.js
+++ zigbee-web/src/main/webapp/app/js/coldStorageComment.js
@@ -4,19 +4,19 @@
 coldWeb.controller('coldStorageComment', function ($rootScope, $scope, $cookies, $http, $location, $state) {
 
     $scope.goColdStorageDetail = function (storageID) {
-        console.log(storageID);
         document.getElementById('detail').setAttribute("class", "active");
         document.getElementById('coldStorageDetail').setAttribute("class", "tab-pane active");
         document.getElementById('comment').removeAttribute("class");
         document.getElementById('coldStorageComment').removeAttribute("class");
         document.getElementById('coldStorageComment').setAttribute("class", "tab-pane");
+        console.log(storageID);
     }
     $scope.goColdStorageComment = function (storageID) {
-        console.log(storageID);
         document.getElementById('comment').setAttribute("class", "active");
         document.getElementById('coldStorageComment').setAttribute("class", "tab-pane active");
         document.getElementById('detail').removeAttribute("class");
         document.getElementById('coldStorageDetail').removeAttribute("class");
         document.getElementById('coldStorageDetail').setAttribute("class", "tab-pane");
+        console.log(storageID);
     }
 });
