71c361fe812491691fa5474599a71a300c7ab2c8
qiunian.sun <qiunian.sun@dianping.com>
2016-04-30 18:22:57 +0800

    add rdc

--- cold-web/src/main/java/com/smartcold/manage/cold/dao/RdcMapper.java
+++ cold-web/src/main/java/com/smartcold/manage/cold/dao/RdcMapper.java
@@ -1,6 +1,9 @@
 package com.smartcold.manage.cold.dao;
 
 import com.smartcold.manage.cold.entity.Rdc;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface RdcMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,8 @@ public interface RdcMapper {
     int updateByPrimaryKeySelective(Rdc record);
 
     int updateByPrimaryKey(Rdc record);
+
+    List<Rdc> findRdcList();
+
+    List<Rdc> findRDCByRDCId(@Param("rdcID") int rdcID);
 }
