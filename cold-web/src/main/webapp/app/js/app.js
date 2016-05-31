545a049ea3b92d3af2ba0a5e028359b003e0a17b
keren.chen <keren.chen@dianping.com>
2016-06-01 00:21:43 +0800

    login

--- cold-web/src/main/webapp/app/js/app.js
+++ cold-web/src/main/webapp/app/js/app.js
@@ -12,7 +12,7 @@ angular.element(document).ready(function($ngCookies, $location) {
 	    	user = data;
 	    	if(user.username == null){
 	    		if(window.location.pathname != "/login.html" && window.location.pathname != '/register.html'){
-	    			document.location.href = "login.html";
+	    			document.location.href = "http://www.smartcold.cn/login.html";
 	    		}
 	        }
 	    	angular.bootstrap(document, ['ColdWeb']);
