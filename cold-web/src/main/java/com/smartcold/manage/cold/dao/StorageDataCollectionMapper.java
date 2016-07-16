9c0a13ce24841cd4f5fdcb4bf0c4f086124fcdb9
keren.chen <keren.chen@dianping.com>
2016-07-16 11:31:46 +0800

    test ap

--- cold-web/src/main/java/com/smartcold/manage/cold/dao/StorageDataCollectionMapper.java
+++ cold-web/src/main/java/com/smartcold/manage/cold/dao/StorageDataCollectionMapper.java
@@ -2,9 +2,13 @@ package com.smartcold.manage.cold.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.smartcold.manage.cold.entity.StorageDataCollectionEntity;
 
 public interface StorageDataCollectionMapper {
 
 	void batchInsert(List<StorageDataCollectionEntity> batchEntity);
+
+	List<StorageDataCollectionEntity> findLastNPoint(@Param("apid") String apid, @Param("limit") int limit);
 }
