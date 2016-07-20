3accb31188f012908f80e3369a82d96a66a94672
maqiang34 <806487943@qq.com>
2016-07-20 09:43:38 +0800

    支持移动终端

--- zigbee-web/src/main/webapp/app/js/app.js
+++ zigbee-web/src/main/webapp/app/js/app.js
@@ -298,3 +298,27 @@ coldWeb.config(function ($stateProvider, $urlRouterProvider) {
     });
 
 });
+
+
+//    var p = navigator.platform; 
+//   var system = {  win: p.indexOf("Win") == 0, mac: p.indexOf("Mac") == 0,  xll: (p == "X11") || (p.indexOf("Linux") == 0), ipad:(navigator.userAgent.match(/iPad/i) != null)?true:false,isComputer:false,isphone:false  }; 
+//    if (system.win || system.mac || system.xll||system.ipad) {  system.isComputer=true;} else {system.isphone=true; } 
+    var browser = { 
+            versions : function() {  var u = navigator.userAgent, app = navigator.appVersion; 
+                return {        
+                trident : u.indexOf('Trident') > -1, //IE内核                                  
+                presto : u.indexOf('Presto') > -1, //opera内核                                  
+                webKit : u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核                                  
+                gecko : u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核                                 
+                mobile : !!u.match(/AppleWebKit.*Mobile.*/)  || !!u.match(/AppleWebKit/), //是否为移动终端                                  
+                ios : !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端                  
+                android : u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器                                  
+                iPhone : u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者QQHD浏览器                     
+                iPad: u.indexOf('iPad') > -1, //是否iPad        
+                webApp : u.indexOf('Safari') == -1,//是否web应该程序，没有头部与底部 
+                google:u.indexOf('Chrome')>-1 
+            }; 
+        }(), 
+        language : (navigator.browserLanguage || navigator.language).toLowerCase() 
+        } ;
+//        document.writeln("语言版本: "+browser.language+" 是否为移动终端: "+browser.versions.mobile); 
