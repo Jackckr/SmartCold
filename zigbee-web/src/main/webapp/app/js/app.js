8ab80a84d806bd422a089058655fd6062ddc1fa6
maqiang34 <806487943@qq.com>
2016-07-23 16:21:27 +0800

    修改手机跳转

--- zigbee-web/src/main/webapp/app/js/app.js
+++ zigbee-web/src/main/webapp/app/js/app.js
@@ -1,23 +1,19 @@
- var browser = { 
-            versions : function() {  var u = navigator.userAgent, app = navigator.appVersion; 
-                return {        
-                trident : u.indexOf('Trident') > -1, //IE内核                                  
-                presto : u.indexOf('Presto') > -1, //opera内核                                  
-                webKit : u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核                                  
-                gecko : u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核                                 
-                mobile : !!u.match(/AppleWebKit.*Mobile.*/)  || !!u.match(/AppleWebKit/), //是否为移动终端                                  
-                ios : !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端                  
-                android : u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器                                  
-                iPhone : u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者QQHD浏览器                     
-                iPad: u.indexOf('iPad') > -1, //是否iPad        
-                webApp : u.indexOf('Safari') == -1,//是否web应该程序，没有头部与底部 
-                google:u.indexOf('Chrome')>-1 
-            }; 
-        }(),  language : (navigator.browserLanguage || navigator.language).toLowerCase() 
-   } ;
-   if(browser.versions.mobile){
-	   window.location.href =  "http://m.liankur.com";
-   }
+function browserRedirect() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    /*document.writeln("您的浏览设备为：");*/
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+        window.location.href='http://m.liankur.com'; /* 链接到不同的网址  这个是手机的 */
+    } 
+}
+browserRedirect();
 var coldWeb = angular.module('ColdWeb', ['ui.bootstrap', 'ui.router', 'ui.checkbox','ngCookies', 'xeditable', 'isteven-multi-select', 'angucomplete', 'angular-table','ngFileUpload','remoteValidation', 'jkuri.gallery']);
 
 angular.element(document).ready(function ($ngCookies, $http, $rootScope) {
