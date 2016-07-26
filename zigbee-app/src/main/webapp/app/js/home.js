bba5c52d57fa8384182eda1bd11f1f8ac96b7575
maqiang34 <806487943@qq.com>
2016-07-26 09:42:51 +0800

    支持手机跳转

--- zigbee-app/src/main/webapp/app/js/home.js
+++ zigbee-app/src/main/webapp/app/js/home.js
@@ -1,5 +1,7 @@
 var app = angular.module('mapp', []);
   app.controller('homecontroller', function( $scope, $http) {
+	  $scope.province=null;
+//	  $scope.shrdcList=null;
     $http.get(ER.root+'i/city/findProvinceList').success(function(data) {  $scope.province=data; });//初始化 城市
     $http.get(ER.root+'i/ShareRdcController/getSERDCList', { params: {datatype:3,pageSize:5}  }).success(function(data) { $scope.shrdcList=data.data; });//初始化发布信息-ur l274
 	$scope.initevg=function(){
