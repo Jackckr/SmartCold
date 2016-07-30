e0d201881fba44f371253871ddba051a92fe1be8
keren.chen <keren.chen@dianping.com>
2016-07-30 13:08:54 +0800

    angucomplete

--- cold-web/src/main/webapp/app/js/app.js
+++ cold-web/src/main/webapp/app/js/app.js
@@ -1,5 +1,5 @@
 var coldWeb = angular.module('ColdWeb', ['ui.bootstrap', 'ui.router', 'ui.checkbox',
-                                         'ngCookies', 'xeditable', 'isteven-multi-select','angucomplete','angular-table']);
+                                         'ngCookies', 'xeditable','angucomplete-alt','angular-table']);
 var user;
 
 angular.element(document).ready(function($ngCookies, $location) {
@@ -63,8 +63,10 @@ coldWeb.factory('userService', ['$rootScope', '$state', '$http', function ($root
             $rootScope.user = user;
         },
         setStorage: function () {
-        	$rootScope.changeRdc = function(){
-        		$rootScope.vm.choserdc = $rootScope.vm.choserdcs[0];
+        	$rootScope.changeRdc = function(value){
+        		if(value){
+            		$rootScope.vm.choserdc = value.originalObject
+        		}
         		$http.get('/i/coldStorageSet/findStorageSetByRdcId?rdcID=' + $rootScope.vm.choserdc.id).success(
         				function(data,status,headers,config){
         					$rootScope.mystorages = data;
@@ -78,6 +80,11 @@ coldWeb.factory('userService', ['$rootScope', '$state', '$http', function ($root
             var compressors = [];
             var mystorages = [];
             if ($rootScope.user != null && $rootScope.user!='' && $rootScope.user!= undefined && $rootScope.user.id != 0){
+            	$http.get('/i/rdc/findRDCByUserid?userid=' + $rootScope.user.id).success(
+            			function(data,status,headers,config){
+            				$rootScope.vm = {choserdc:data[0]}
+            			})
+            	
                 // 拉取压缩机组列表
                 $http.get('/i/compressorGroup/findByUserId', {
                     params: {
@@ -111,19 +118,19 @@ coldWeb.factory('userService', ['$rootScope', '$state', '$http', function ($root
 
                     $rootScope.mystorages = mystorages;
                     $rootScope.rdcId = result[0].rdcId;
-                    if($rootScope.user.role == 1){
-                    $http.get('/i/rdc/findRdcList').success(function(data,status,headers,config){
-                		$rootScope.rdcs = data;
-                		angular.forEach(data,function(item){
-                			if(item.id == $rootScope.rdcId){
-                				item.isOn = true;
-                				$rootScope.vm = {choserdc : item};
-                			}else{
-                				item.isOn = false;
-                			}
-                		})
-                	})
-                    }
+//                    if($rootScope.user.role == 1){
+//                    $http.get('/i/rdc/findRdcList').success(function(data,status,headers,config){
+//                		$rootScope.rdcs = data;
+//                		angular.forEach(data,function(item){
+//                			if(item.id == $rootScope.rdcId){
+//                				item.isOn = true;
+//                				$rootScope.vm = {choserdc : item};
+//                			}else{
+//                				item.isOn = false;
+//                			}
+//                		})
+//                	})
+//                    }
                 })
             }
 
