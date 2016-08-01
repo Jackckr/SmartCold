1f29ac4f4c53562a19bfe4af3f1165a89febfff2
MengShuang <corlymeng@qq.com>
2016-08-01 14:53:30 +0800

    modify history data

--- cold-web/src/main/webapp/app/js/historyData.js
+++ cold-web/src/main/webapp/app/js/historyData.js
@@ -1,10 +1,15 @@
-coldWeb.controller('report', function ($scope, $location,$stateParams,$timeout,$http,$rootScope) {
+coldWeb.controller('historyData', function ($scope, $http,$rootScope) {
 	$scope.getDateTimeStringBefore = function(before){
 		return new Date(new Date().getTime() - before *24*60*60*1000).toISOString().replace("T"," ").replace(/\..*/g,'');
 	}
-	$http.get("/i/baseInfo/findAllStorageType").then(function(response){
-		$scope.searchOptions = response.data;
-		$scope.choseOption = $scope.searchOptions[0];
+	console.log($rootScope.vm);
+	$http.get("/i/historySearch/findAllStorageKeys",{
+		params:{
+			'rdcId':$rootScope.vm.choserdc.id
+		}
+	}).success(function(response){
+		$scope.searchOptions = response;
+		$scope.selectedOption = $scope.searchOptions[0];
 	})
 
 	$scope.begin = $scope.getDateTimeStringBefore(3);
@@ -12,4 +17,25 @@ coldWeb.controller('report', function ($scope, $location,$stateParams,$timeout,$
 	$scope.picktime = $scope.begin + ' - ' + $scope.end;
 	$('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 1, format: 'YYYY-MM-DD HH:mm:ss'});
 	
+	
+	$scope.search = function(){
+		var selected = $scope.selectedOption;
+		if(selected){
+			console.log(selected.keyDesc);
+			bothTime = $scope.picktime.split(" - ");
+			$scope.begin = bothTime[0];
+			$scope.end = bothTime[1];
+			$http.get("i/baseInfo/getKeyValueDataByTime",{
+				params:{
+					type:selected.type,
+					oid:selected.oid,
+					key:selected.key,
+					startTime:$scope.begin,
+					endTime:$scope.end  
+				}
+			}).then(function(response){
+				
+			})
+		}
+	}
 });
