1e716b4fc523ce871b97c008f973e09710270fe6
MengShuang <corlymeng@qq.com>
2016-08-05 10:37:27 +0800

    add js file

--- zigbee-web/src/main/java/com/smartcold/zigbee/manage/filter/CrossInterceptor.java
+++ zigbee-web/src/main/java/com/smartcold/zigbee/manage/filter/CrossInterceptor.java
@@ -19,8 +19,8 @@ public class CrossInterceptor extends OncePerRequestFilter {
 //	        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
 	            // CORS "pre-flight" request
 	            response.addHeader("Access-Control-Allow-Origin", "*");
-	            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+	            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	            response.addHeader("Access-Control-Allow-Headers", "Content-Type,Content-Transfer-Encoding");
 	            response.addHeader("Access-Control-Max-Age", "1800");//30 min
 //	        }
 	        filterChain.doFilter(request, response);
