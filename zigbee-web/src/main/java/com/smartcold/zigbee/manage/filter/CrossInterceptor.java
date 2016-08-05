8cc0fe53ea2d208a068211848028b59151f40561
MengShuang <corlymeng@qq.com>
2016-08-05 16:51:15 +0800

    null

--- zigbee-web/src/main/java/com/smartcold/zigbee/manage/filter/CrossInterceptor.java
+++ zigbee-web/src/main/java/com/smartcold/zigbee/manage/filter/CrossInterceptor.java
@@ -18,9 +18,10 @@ public class CrossInterceptor extends OncePerRequestFilter {
 	    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 //	        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
 	            // CORS "pre-flight" request
-	            response.addHeader("Access-Control-Allow-Origin", "*");
+	            response.addHeader("Access-Control-Allow-Origin", "http://localhost:8081");
 	            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 	            response.addHeader("Access-Control-Allow-Headers", "Content-Type,Content-Transfer-Encoding");
+	            response.addHeader("Access-Control-Allow-Credentials", "true"); 
 	            response.addHeader("Access-Control-Max-Age", "1800");//30 min
 //	        }
 	        filterChain.doFilter(request, response);
