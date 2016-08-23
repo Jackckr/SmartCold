37f634c336ae59c2eced5329dfd4215b0ffbe5df
maqiang34 <806487943@qq.com>
2016-08-23 14:12:56 +0800

    修改app

--- zigbee-app/src/main/webapp/com/js/common.js
+++ zigbee-app/src/main/webapp/com/js/common.js
@@ -2,10 +2,10 @@
 var oHtml = document.documentElement;
 var _sysconfig={countdown:60,isdebug:true};
 var screenWidth = oHtml.clientWidth,screenHeight = oHtml.clientHeight;
-var ER = {root:"http://liankur.com",coldroot:"http://www.smartcold.org.cn"};
-//var ER = {root:"http://192.168.1.136:8080",coldroot:"http://www.smartcold.org.cn",isdebug:true};
+//var ER = {root:"http://liankur.com",coldroot:"http://www.smartcold.org.cn"};
+var ER = {root:"http://192.168.1.136:8080",coldroot:"http://www.smartcold.org.cn",isdebug:true};
 if ($.ajax) {jQuery.ajaxSetup({xhrFields:{withCredentials:true}});}
-function goback() {window.history.back();}//返回上一级
+function goback() { if(window.location.pathname.indexOf("login.html")&&window.location.hash.indexOf("user-")!=-1){window.location.href ="user.html";}else{ window.history.back();}}//返回上一级
 function backDropTop(ops){$('.topFirst').hide();}
 function tourl(url){window.location.href =url;}//去指定的url
 function gohome(){window.location.href ="../index.html";};//去首页
@@ -61,7 +61,7 @@ function checkLogin(msg,callback) {
 	                window.user = data;
 	                if(callback){callback(); }
 	            } else {
-	            	alert(msg?msg:"请登录后再操作~");
+//	            	alert(msg?msg:"请登录后再操作~");
 	                window.user = null;
 	                window.location.href = "login.html#" + window.location.href;
 	                return;
