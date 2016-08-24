c9f050d4444b91678920ab763e7c63b4180398da
maqiang34 <806487943@qq.com>
2016-08-24 17:21:29 +0800

    修改app

--- zigbee-app/src/main/webapp/app/js/colddetail.js
+++ zigbee-app/src/main/webapp/app/js/colddetail.js
@@ -36,12 +36,30 @@
 			});
 		};
 		$scope.initevg=function(){
-			 baguetteBox.run('.baguetteBoxOne', {
-	             animation: 'slideIn',
-	             buttons: true
-	         });
+			 baguetteBox.run('.baguetteBoxOne', {animation: 'slideIn', buttons: true});
 		};
 		$scope.initdata();
 		$scope.initevg();
+		$scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+			alert("22");
+	          //下面是在table render完成后执行的js
+	          var table = $("#leaderBoard").dataTable({
+	              bJQueryUI: true,
+	              "sScrollX": '100%',
+	          });
+	   });
       });
+      app.directive('onFinishRenderFilters', function ($timeout) {
+    	    return {
+    	        restrict: 'A',
+    	        link: function(scope, element, attr) {
+    	            if (scope.$last === true) {
+    	            	alert("2");
+    	                $timeout(function() {
+    	                    scope.$emit('ngRepeatFinished');
+    	                });
+    	            }
+    	        }
+    	    };
+    	})
  }
