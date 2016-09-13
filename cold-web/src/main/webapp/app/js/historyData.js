70fd0d1c08e4aadf66fcb3fa03634ae482c62b0e
maqiang34 <806487943@qq.com>
2016-09-13 15:21:44 +0800

    修改js

--- cold-web/src/main/webapp/app/js/historyData.js
+++ cold-web/src/main/webapp/app/js/historyData.js
@@ -1,8 +1,7 @@
-coldWeb.controller('historyData', function ($scope, $http,$stateParams) {
+coldWeb.controller('historyData', function ($scope, $http,$rootScope) {
 	clearInterval($rootScope.timeTicket);
 	var lineChart =null;
-	console.log($stateParams.rdcId);
-	var rdcid=$stateParams.rdcId;// 
+	var rdcid=window.sessionStorage.smrdcId;//
 	$http.get("/i/historySearch/findStorageKeysByFilter",{params:{'rdcId':rdcid,types:'1,2'}}).success(function(data){$scope.keylist = data.key;$scope.keydata = data.keydata;});
 	$scope.getDateTimeStringBefore = function(before){ return new Date(new Date().getTime() - before *24*60*60*1000).toISOString().replace("T"," ").replace(/\..*/g,''); };
 	$scope.begin = $scope.getDateTimeStringBefore(3);
