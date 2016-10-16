694b972d2d67e239460bad55c54ed1d5f4ffd91b
qiunian.sun <qiunian.sun@dianping.com>
2016-10-16 21:48:58 +0800

    update alarm

---
+++ zigbee-app/src/main/webapp/app/js/alarmLog.js
@@ -0,0 +1,68 @@
+checkLogin();
+var app = angular.module('app', []);
+app.controller('alarmLog', function ($scope, $location, $http) {
+    $scope.user = window.user;
+    $http.defaults.withCredentials = true;
+    $http.defaults.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+    $scope.searchUrl = ER.coldroot + "/i/rdc/searchRdc?filter=";
+    $scope.alarmTotalCnt = 0;
+    $scope.alarmMsgs = [];
+
+    $.getUrlParam = function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]);
+        return null;
+    }
+    var rootRdcId = $.getUrlParam('storageID');
+
+    $http.get(ER.coldroot + '/i/rdc/findRDCsByUserid?userid=' + window.user.id).success(function (data) {
+        if (data && data.length > 0) {
+            $scope.storages = data;
+            if (rootRdcId == undefined || rootRdcId == null) {
+                $scope.currentRdc = $scope.storages[0];
+                $scope.rdcId = $scope.storages[0].id;
+                $scope.rdcName = $scope.storages[0].name;
+                $scope.viewStorage($scope.storages[0].id);
+            } else {
+                $http.get(ER.coldroot + '/i/rdc/findRDCByRDCId?rdcID=' + rootRdcId).success(function (data) {
+                    $scope.currentRdc = data[0];
+                    $scope.rdcName = data[0].name;
+                    $scope.rdcId = data[0].id;
+                    $scope.viewStorage($scope.rdcId);
+                });
+            }
+        }
+    });
+
+    $scope.viewStorage = function (rdcId) {
+        //根据rdcid查询该rdc的报警信息
+        $http.get('/i/warlog/findWarningLogsByRdcID', {
+            params: {
+                "rdcId": rdcId
+            }
+        }).success(function (data) {
+            if (data && data.length > 0) {
+                $scope.alarmTotalCnt = data.length;
+                $scope.alarmMsgs = data;
+            }
+        });
+    }
+    $scope.searchRdcs = function (searchContent) {
+        // 超管特殊处理
+        if ($scope.user.roleid == 3) {
+            $http.get(ER.coldroot + '/i/rdc/searchRdc?filter=' + searchContent).success(function (data) {
+                if (data && data.length > 0) {
+                    $scope.storages = data;
+                }
+            });
+        }
+    }
+    $scope.changeRdc = function (rdc) {
+        $scope.rdcId = rdc.id;
+        $scope.rdcName = rdc.name;
+        $scope.searchContent = "";
+        $scope.alarmTotalCnt = 0;
+        $scope.viewStorage(rdc.id);
+    }
+});
