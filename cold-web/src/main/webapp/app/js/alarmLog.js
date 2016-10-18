90200e8ea04fc014819d9fe4d36991e26980a4f3
maqiang34 <806487943@qq.com>
2016-10-18 16:33:11 +0800

    merge

--- cold-web/src/main/webapp/app/js/alarmLog.js
+++ cold-web/src/main/webapp/app/js/alarmLog.js
@@ -1,17 +1,7 @@
-coldWeb.controller('alarmLog', function($rootScope, $scope, $http,baseTools,$timeout) {
-	/*$scope.alarmMsgs = [ {
-		Msg : '电流不平衡度报警值1',
-		addTime : '2016-10-11 13:29:36'
-	}, {
-		Msg : '电流不平衡度报警值2',
-		addTime : '2016-10-11 23:30:13'
-	}, {
-		Msg : '电流不平衡度报警值3',
-		addTime : '2016-10-12 9:45:29'
-	}, {
-		Msg : '电流不平衡度报警值4',
-		addTime : '2016-10-12 18:20:05'
-	} ];*/
+/**
+ * 　　报警信息
+ */
+coldWeb.controller('alarmLog', function( $scope, $http) {
 	 //根据rdcid查询该rdc的报警信息
         $http.get('/i/warlog/findWarningLogsByRdcID', {
             params: {
