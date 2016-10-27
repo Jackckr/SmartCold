8983ce25ff649ce2a786da6ce40db39043b66527
maqiang34 <806487943@qq.com>
2016-10-27 15:33:19 +0800

    报表支持货物因子和制冷系统

--- cold-web/src/main/webapp/app/js/reportsAnalysis.js
+++ cold-web/src/main/webapp/app/js/reportsAnalysis.js
@@ -11,10 +11,10 @@ coldWeb.controller('reportsAnalysis', function ($scope, $http,$stateParams,$root
 	//key
 	var typemode={
 			      type:[10,3,2,1,1,4,-1,1,5],
-			      unit:[null,null,[1,60,60],[60,1,1,1],null,[3600,3600],null,null,null],//换算单位
+			      unit:[null,null,[1,60,60],[60,1,1,1],null,[3600,3600],null,null,[3600,1,1]],//换算单位
 			      unite:["　(kW·h)","　(T)","","","","","","",""],
 			      title:["电量","水耗","冷库门","温度分析","热量","冷风机","系统效率","货物因子","制冷运行分析"],
-			      key:["'TotalPWC'","'WaterCost'","'OpenTimes','TotalTime','AvgTime';次数,时长(min),平均时长(min)","'ChaoWenShiJian','MaxTemp','ChaoWenYinZi','BaoWenYinZi';超温时长(min),最高温度(℃),超温因子(ε),保温因子(τ)","'GoodsHeat','QFrost','QForklift','Qlighting','WallHeat','Qblower','Qctdoor';Q货,Q霜,Q叉,Q照,Q保,Q风,Q门","'RunningTime','DefrosingTime';制冷时间(H),化霜时间(H)","","'GoodsLiuTongYinZi'","'RunningTime','RunningCount';运行总时间,运行总次数"] 
+			      key:["'TotalPWC'","'WaterCost'","'OpenTimes','TotalTime','AvgTime';次数,时长(min),平均时长(min)","'ChaoWenShiJian','MaxTemp','ChaoWenYinZi','BaoWenYinZi';超温时长(min),最高温度(℃),超温因子(ε),保温因子(τ)","'GoodsHeat','QFrost','QForklift','Qlighting','WallHeat','Qblower','Qctdoor';Q货,Q霜,Q叉,Q照,Q保,Q风,Q门","'RunningTime','DefrosingTime';制冷时间(H),化霜时间(H)","","'GoodsLiuTongYinZi'","'RunningTime','RunningCount','avg';运行总时间(H),运行总次数,平均时间(H)"] 
 	};
     function gettbcltit(value,cl){//获取标题1
 	    if(value==null||value==''||value=='null')return '<td  colspan="'+cl+'" ></td>';else return '<td colspan="'+cl+'">'+value+'</td>';
@@ -90,7 +90,7 @@ coldWeb.controller('reportsAnalysis', function ($scope, $http,$stateParams,$root
 		$.ajax({
             type: "POST",
             url:'i/AnalysisController/getCasesTotalSISAnalysis',
-            data:{index:$scope.urlid,isexpt:isexpt,type:typemode.type[$scope.slindex],confdata:datainfo, key:typemode.key[$scope.slindex],unit:typemode.unit[$scope.slindex], startTime:stentime[0],endTime:stentime[1]},//
+            data:{index:$scope.slindex,urlid:$scope.urlid,isexpt:isexpt,type:typemode.type[$scope.slindex],confdata:datainfo, key:typemode.key[$scope.slindex],unit:typemode.unit[$scope.slindex], startTime:stentime[0],endTime:stentime[1]},//
             success: function(data) {
                 if(data.success){
                 	isSuccess=true;
