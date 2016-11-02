2df9677d04f082696861d252f3a06a61840cf074
maqiang34 <806487943@qq.com>
2016-11-02 12:09:02 +0800

    apk下载

--- zigbee-web/src/main/java/com/smartcold/zigbee/manage/controller/UtilController.java
+++ zigbee-web/src/main/java/com/smartcold/zigbee/manage/controller/UtilController.java
@@ -1,8 +1,14 @@
 package com.smartcold.zigbee.manage.controller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,7 +21,11 @@ import com.smartcold.zigbee.manage.dto.RdcShareDTO;
 import com.smartcold.zigbee.manage.service.RdcService;
 import com.smartcold.zigbee.manage.service.RdcShareService;
 import com.smartcold.zigbee.manage.util.ResponseData;
-
+/*
+ * Copyright (C) DCIS 版权所有
+ * 功能描述: Utils 工具类->全文检索
+ * Create on MaQiang 2016-6-25 09:28:36
+ */
 @Controller
 @RequestMapping(value = "/UtilController")
 public class UtilController   {
@@ -50,4 +60,52 @@ public class UtilController   {
 			return ResponseData.newFailure("抱歉！暂时没有找到相关信息！");
 		}
 	}
+	
+	@RequestMapping(value = "/downLoadapk")
+	@ResponseBody
+	public void downLoadapk(HttpServletRequest request, HttpServletResponse response){
+		try {
+			this.downLoad(response,"apk/liankur.v1.0.3.apk",false);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * 文件下载
+	 * @param filePath
+	 * @param response
+	 * @param isOnLine
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/downLoad")
+	@ResponseBody
+	public void downLoad( HttpServletResponse response,String filePath, Boolean isOnLine) throws Exception {
+        File f = new File(filePath);
+        if (!f.exists()) {
+            response.sendError(404, "File not found!");
+            return;
+        }
+        BufferedInputStream br = new BufferedInputStream(new FileInputStream(f));
+        byte[] buf = new byte[1024];
+        int len = 0;
+
+        response.reset(); // 非常重要
+        if (isOnLine!=null&&isOnLine) { // 在线打开方式
+            URL u = new URL("file:///" + filePath);
+            response.setContentType(u.openConnection().getContentType());
+            response.setHeader("Content-Disposition", "inline; filename=" + f.getName());
+            // 文件名应该编码成UTF-8
+        } else { // 纯下载方式
+            response.setContentType("application/x-msdownload");
+            response.setHeader("Content-Disposition", "attachment; filename=" + f.getName());
+        }
+        OutputStream out = response.getOutputStream();
+        while ((len = br.read(buf)) > 0)
+            out.write(buf, 0, len);
+        br.close();
+        out.close();
+    }
 }
