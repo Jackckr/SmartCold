e26f97170eddceef917eb37a69580b7f30c588aa
weiwandaixu <jiang997160328@outlook.com>
2016-11-19 14:54:29 +0800

    bug fix -app red point

--- zigbee-app/src/main/webapp/com/js/common.js
+++ zigbee-app/src/main/webapp/com/js/common.js
@@ -3,17 +3,17 @@ var oHtml = document.documentElement;
 var _sysconfig={countdown:60,isdebug:true,resize:true};
 var screenWidth = oHtml.clientWidth,screenHeight = oHtml.clientHeight;
 getFont();$(window).resize(function(event) { if(_sysconfig.resize)getFont();});
-var ER = {root:"http://liankur.com",coldroot:"http://www.smartcold.org.cn"};
+var ER = {root:"http://192.168.1.100:8989",coldroot:"http://www.smartcold.org.cn"};
+//var ER = {root:"http://liankur.com",coldroot:"http://www.smartcold.org.cn"};
 if ($.ajax) {jQuery.ajaxSetup({xhrFields:{withCredentials:true}});}
 if(window.localStorage.msgTotalNum==undefined){
 	window.localStorage.msgTotalNum = 2;
 }
 if(localStorage.length>=14){for(var i in localStorage ){if(i.indexOf("BMap_")>=0){ localStorage.removeItem(i);}}}
 if(window.user==undefined ||window.user==null){var userjson=window.localStorage.lkuser;if(userjson){window.user=JSON.parse(userjson);userjson=undefined;}}
-if(window.user!=undefined ||window.user!=null){}
-
+/*if(window.user!=undefined ||window.user!=null)
 //暂时隐藏
-/*{
+{
  $.post(ER.root+"/i/message/findMessageByUserId", {
      pageNum: null,
      pageSize:null,
