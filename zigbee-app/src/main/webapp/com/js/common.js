4446ed9772dde58a74702e007e7a60bbe545560b
wellsea <857885610@qq.com>
2016-12-13 14:55:50 +0800

    style

--- zigbee-app/src/main/webapp/com/js/common.js
+++ zigbee-app/src/main/webapp/com/js/common.js
@@ -3,9 +3,9 @@ var oHtml = document.documentElement;
 var _sysconfig={countdown:60,isdebug:true,resize:true};
 var screenWidth = oHtml.clientWidth,screenHeight = oHtml.clientHeight;
 getFont();$(window).resize(function(event) { if(_sysconfig.resize)getFont();});
-var ER = {root:"http://liankur.com",coldroot:"http://www.smartcold.org.cn"};
+//var ER = {root:"http://liankur.com",coldroot:"http://www.smartcold.org.cn"};
 //var ER = {root:"http://192.168.1.137:8080/",coldroot:"http://www.smartcold.org.cn"};
-//var ER = {root:"http://192.168.1.114:8080/",coldroot:"http://192.168.1.114:8989/"};
+var ER = {root:"http://192.168.1.114:8080/",coldroot:"http://192.168.1.114:8989/"};
 if ($.ajax) {jQuery.ajaxSetup({xhrFields:{withCredentials:true}});}//支持ajax跨域
 if(localStorage.length>=14){for(var i in localStorage ){if(i.indexOf("BMap_")>=0){ localStorage.removeItem(i);}}}
 if(window.user==undefined ||window.user==null){var userjson=window.localStorage.lkuser;if(userjson){window.user=JSON.parse(userjson);userjson=undefined;}}
