ecd63b306513b2386efb41cc0a1cfdc804acf050
wellsea <857885610@qq.com>
2017-01-05 18:04:58 +0800

    pc我的消息详情

--- zigbee-web/src/main/webapp/app/js/personalMessage.js
+++ zigbee-web/src/main/webapp/app/js/personalMessage.js
@@ -32,6 +32,42 @@ coldWeb.controller('personalMessage', function ($rootScope, $scope, $state, $coo
 		$scope.getRdcs();
 	}
 	$scope.getMsgs();
+	$scope.goShowMsg = function (obj) {
+		console.log(obj+'------'+obj.oid)
+	   $scope.appmode=[{title:["","货品","配送","冷库"]}];//1:货品 2：配送 3:仓库
+	   $http.get("/i/orders/findOrderByOrderId",  { params: {id:obj.oid,uid:obj.userid}  })
+	   .success(function(data) { //获得数据
+		 if(data.success){
+			   $scope.rsd=data.entity.rsd; //发布的信息
+			   $scope.orders=data.entity.orders; //联络单信息
+			   $scope.ownerUser=data.entity.ownerUser; //发布者信息
+			   $scope.orderUser=data.entity.orderUser; //联络单者信息
+			   $scope.orders.generatetime=formatCSTDate($scope.orders.generatetime,"yyyy-MM-dd hh:mm:ss")
+			    if($scope.rsd){
+			       $('#order_Modal').modal('show');
+			       $scope.datatype=$scope.rsd.dataType;
+			       $scope.typetitle=$scope.appmode[0].title[$scope.datatype]+data.entity.rsd.typeText;
+			    }else{
+			      alert("商品信息已失效！");
+			    }
+		   }else{
+		       alert("获取数据失败！");// goback();
+		   }
+       });
+	}
+	$scope.getTeleNum = function () {
+    	$http.get("/i/orders/getTelephone", {
+    	    params: {
+    	        orderid : $scope.orders.id,
+    	    	ownerTele: $scope.orders.ownertele,
+    	    	userTele : $scope.orders.usertele,
+    	    	ownerName : $scope.orders.ownername,
+    	    	userName : $scope.orders.username
+    	    }
+    	}).success(function (data) {
+     		alert("对方联系人的手机号已经发送到您的手机，请及时联系");
+    	});
+    };
 	$scope.deleteMsg = function(msgID){
 	    	var r=confirm("删除消息？");
 	    	if(r){
