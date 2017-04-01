6aef8b4937112d4b9bbb53cbd2199e82a351cf43
jhy <wellsea81>
2017-04-01 17:27:17 +0800

    权限style

--- bgzigbee-web/src/main/webapp/app/js/index.js
+++ bgzigbee-web/src/main/webapp/app/js/index.js
@@ -30,6 +30,19 @@ function secLi(ops,url){
 	window.location.href=url;
 }
 window.onresize = function(){
+	var view_width = window.innerWidth;
 	$(".sidebar").height(0);
 	$(".sidebar").height($(document).height());
+	//console.log(view_width)
+	if(view_width<768){
+		$(".sidebar").hide();
+		$(".side_toggle").show();
+	}else{
+		$(".sidebar").show();
+		$(".side_toggle").hide();
+	}
 }
+$(".side_toggle").click(function(){
+	$(this).hide();
+	$(".sidebar").show();
+})
