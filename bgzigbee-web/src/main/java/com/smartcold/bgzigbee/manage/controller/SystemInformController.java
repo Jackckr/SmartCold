b8de5e67597e158d24c1d8d4d8e5ec77873b1b8b
maqiang34 <806487943@qq.com>
2017-05-08 11:13:40 +0800

    merge

--- bgzigbee-web/src/main/java/com/smartcold/bgzigbee/manage/controller/SystemInformController.java
+++ bgzigbee-web/src/main/java/com/smartcold/bgzigbee/manage/controller/SystemInformController.java
@@ -3,6 +3,8 @@ package com.smartcold.bgzigbee.manage.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smartcold.bgzigbee.manage.dao.SystemInformMapper;
 
@@ -16,5 +18,13 @@ public class SystemInformController {
 	@Autowired
 	private SystemInformMapper systemInformMapper;
 	
+	@RequestMapping(value = "/getSystemInform", method = RequestMethod.POST)
+	@ResponseBody
+	public Object getObjByType(Integer type,Integer stype, String keyword, Integer pageNum,Integer pageSize ) {
+		
+		
+		
+		return null;
+	}
 	
 }
