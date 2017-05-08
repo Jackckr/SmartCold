b95e1ef3b06eb6033c952313d0c95d83cf2dd907
maqiang34 <806487943@qq.com>
2017-05-08 11:07:44 +0800

    merge

---
+++ bgzigbee-web/src/main/java/com/smartcold/bgzigbee/manage/dao/SystemInformMapper.java
@@ -0,0 +1,12 @@
+package com.smartcold.bgzigbee.manage.dao;
+
+/**
+ * 
+ *@author Kaiqiang Jiang
+ *@date:2016-6-22 上午11:11:51
+ *@Description: Admin Mapper
+ */
+public interface SystemInformMapper {
+
+	
+}
