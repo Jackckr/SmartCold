48002a2bdf013958f9e021b4234bfdf1c706fb92
wellsea <857885610@qq.com>
2017-05-21 14:37:25 +0800

    登录认证更新

--- cold-web/src/main/java/com/smartcold/manage/cold/controller/LoginStepController.java
+++ cold-web/src/main/java/com/smartcold/manage/cold/controller/LoginStepController.java
@@ -1,7 +1,9 @@
 package com.smartcold.manage.cold.controller;
 
 import com.smartcold.manage.cold.dao.olddb.*;
+import com.smartcold.manage.cold.dto.UploadFileEntity;
 import com.smartcold.manage.cold.entity.olddb.*;
+import com.smartcold.manage.cold.service.FtpService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,10 +21,15 @@ import java.util.Date;
 @Controller
 @RequestMapping(value = "/loginStep")
 public class LoginStepController {
+    private static String baseDir = "picture";
     @Resource
     private RdcMapper rdcMapper;
     @Resource
     private UserMapper userMapper;
+    @Resource
+    private FtpService ftpService;
+    @Resource
+    private FileDataMapper fileDataDao;
     @Resource
     private MessageRecordMapping messageRecordMapping;
     @Resource
@@ -40,19 +47,34 @@ public class LoginStepController {
 
     @RequestMapping(value = "/attestationRdc",method = RequestMethod.POST)
     @ResponseBody
-    public void attestationRdc(
-                                 @RequestParam(value = "rdcId", required = false) Integer rdcId,
-                                 @RequestParam(value = "roleType", required = false) Integer roleType,
-                                 HttpSession session) {
+    public void attestationRdc( MultipartFile authfile0,
+                                @RequestParam(value = "rdcId", required = false) Integer rdcId,
+                                @RequestParam(value = "roleType", required = false) Integer roleType,
+                                HttpSession session) {
         UserEntity user = (UserEntity) session.getAttribute("user");
         user.setType(roleType);
         userMapper.updateTypeById(user);
-        if (roleType==3){
+
+        if (roleType==0){
+            MultipartFile authfile = authfile0;
+            String filename = authfile.getOriginalFilename();
             ColdStorageCertification coldStorageCertification = new ColdStorageCertification();
             coldStorageCertification.setUid(user.getId());
             coldStorageCertification.setRdcId(rdcId);
+            coldStorageCertification.setCertFile(filename);
             coldStorageCertification.setAddTime(new Date());
             coldStorageCertificationMapping.insertCertification(coldStorageCertification);
+            Rdc rdcEntity = rdcMapper.findRDCByRDCId(rdcId).get(0);
+            String dir = String.format("%s/rdc/%s", baseDir, rdcId);
+            if (authfile != null) {
+                String fileName = String.format("rdc%s_%s_%s.%s", rdcId, user.getId(), new Date().getTime(), "jpg");
+                UploadFileEntity uploadFileEntity = new UploadFileEntity(fileName, authfile, dir);
+                ftpService.uploadFile(uploadFileEntity);
+                FileDataEntity arrangeFile = new FileDataEntity(authfile.getContentType(), dir + "/" + fileName,
+                        FileDataMapper.CATEGORY_AUTH_PIC, rdcEntity.getId(), fileName);
+                if(user!=null){arrangeFile.setDescription(user.getType()+"");}//标志为服务商
+                fileDataDao.saveFileData(arrangeFile);
+            }		// 上传认证后更改冷库审核状态为待审核
         }else {
             MessageRecord messageRecord = new MessageRecord();
             messageRecord.setUid(user.getId());
