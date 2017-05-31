68dcd8c20edaa0f842390c4e58ff9246d3f14a5f
wellsea <857885610@qq.com>
2017-05-31 17:18:18 +0800

    app冷库认证

--- zigbee-app/src/main/webapp/app/js/coldRole.js
+++ zigbee-app/src/main/webapp/app/js/coldRole.js
@@ -1,5 +1,6 @@
 checkLogin();
-app.controller('coldRole',function ($scope, $location, $http, $rootScope, userService) {
+app.controller('coldRole',function ($scope, $location, $http, $rootScope,Upload,userService) {
+    $scope.totalauthfiles = [];
 	$scope.user = user;
     $http.defaults.withCredentials = true;
     $http.defaults.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
@@ -10,11 +11,45 @@ app.controller('coldRole',function ($scope, $location, $http, $rootScope, userSe
     $scope.goStep1 = function () {
         $scope.step = 1;
     };
-    $scope.goStep2 = function () {
+    $scope.goStep2 = function (type) {
+        $scope.user.type=type;
+        if (type!=0){
+            $scope.selImgFlag=true;
+        }else {
+            $scope.selImgFlag=false;
+        }
         $scope.step = 2;
     };
     $scope.goStep3 = function () {
-        $scope.step = 3;
+        var flag=true;
+        if(!$scope.rdcId){
+            flag=false;
+        }
+        if($scope.user.type==0&&$scope.totalauthfiles.length==0){
+            flag=false;
+        }
+        if (flag){
+            var data = {
+                rdcId:$scope.rdcId,
+                type:$scope.user.type,
+                userId:$scope.user.id,
+                userName:$scope.user.username
+            };
+            if($scope.user.type==0){
+                data.authfile = $scope.totalauthfiles[0];
+            }
+            Upload.upload({
+                data: data,
+                url: ER.coldroot+'/i/authenUser/attestationRdc',
+                headers: {'Content-Transfer-Encoding': 'utf-8'}
+            }).then(function (resp) {
+                $scope.isDisabled = false;
+                $("#auCode").html(resp.data.message);
+            });
+            $scope.step = 3;
+        }else {
+            alert("请完善冷库认证信息！");
+        }
     };
     $scope.goStep4 = function () {
         $scope.step = 4;
@@ -39,6 +74,36 @@ app.controller('coldRole',function ($scope, $location, $http, $rootScope, userSe
             });
         }
     };
+    $scope.addAuthFiles = function (files) {
+        for(var j=0,fileLen=files.length;j<fileLen;j++){
+            var _file=files[j].name;
+            var i=_file.lastIndexOf('.');
+            var len=_file.length;
+            var extEndName=_file.substring(i+1, len);
+            var extName="GIF,BMP,JPG,JPEG,PNG";
+            //首先对格式进行验证
+            if(extName.indexOf(extEndName.toUpperCase())==-1) {
+                alert("只能上传"+extName+"格式的文件");
+                return false;
+            }else if(files[j].size > 10485760){
+                alert("最大只能上传10M的图片");
+                return false;
+            }else{
+                if ($scope.totalauthfiles.length + files.length > 1) {
+                    $scope.totalauthfiles = [];
+                }
+                $scope.totalauthfiles = $scope.totalauthfiles.concat(files);
+            }
+        }
+    };
+    $scope.dropauth = function (authfile) {
+        angular.forEach($scope.totalauthfiles, function (item, key) {
+            if (item == authfile) {
+                $scope.totalauthfiles.splice(key, 1);
+                return false;
+            }
+        })
+    }
     $scope.changeRdc = function (rdc) {
         $(".searchTop").hide();
         $scope.step = 2;
