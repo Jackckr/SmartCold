4f666432774d55a416fd0a33f6b691788db34efd
jhy <wellsea81>
2017-06-05 13:33:56 +0800

    h5 360温度报警

--- cold360/src/main/webapp/app/js/alarmLog.js
+++ cold360/src/main/webapp/app/js/alarmLog.js
@@ -1,12 +1,49 @@
 checkLogin();
 app.controller('alarmLog', function ($scope, $location, $http, $rootScope, userService) {
     $scope.user = window.user;
+    var myChart = echarts.init(document.getElementById('tem_div'));
     $http.defaults.withCredentials = true;
     $http.defaults.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
     $scope.searchUrl = ER.coldroot + "/i/rdc/searchRdc?type=1&filter=";
     $scope.alarmTotalCnt = 0;
     $scope.alarmMsgs = [];
 
+    $(".mylog").click(function (event) {
+        var _index = $(this).index();
+        $('.warn_sec>div').eq(_index).show().siblings().hide();
+        $(this).addClass('current').siblings().removeClass('current');
+    })
+    $scope.initData=function(){
+        var oids=""; angular.forEach($rootScope.mystorages,function(storage){ oids+=storage.id+",";  });
+        oids=oids.substr(0,oids.length-1);
+        $http.get(ER.coldroot +'/i/AlarmController/getOverTempAnalysis', {  params: { "oids": oids  } }).success(function (data) {
+            $scope.tempwarLog=[];
+            if(data.success){
+                var datalist=  data.entity,xAxis=[],count=[],time=[];
+                angular.forEach(datalist,function(item,i){
+                    xAxis.push(i);
+                    time.push(item[0]);
+                    count.push(item[1]);
+                    if(item[1]>0){
+                        var msg=new Object();
+                        msg.time=i;
+                        msg.style="background:#ED3F1D";
+                        msg.count=item[1];
+                        msg.msg="当前所有冷库累计"+item[1]+"次告警,累计时长："+item[0]+"分钟";
+                        $scope.tempwarLog.push(msg);
+                    }
+                });
+                if( $scope.tempwarLog.length==0){
+                    var msg=new Object();
+                    msg.time=time[7];
+                    msg.msg="暂无告警信息";
+                    msg.count=0;
+                    $scope.tempwarLog.push(msg);
+                }
+                $scope.dwechar(xAxis, count, time);
+            }
+        });
+    };
     $.getUrlParam = function (name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
         var r = window.location.search.substr(1).match(reg);
@@ -33,7 +70,16 @@ app.controller('alarmLog', function ($scope, $location, $http, $rootScope, userS
                 findByRdcId(rootRdcId);
             }
         }
+        $scope.changeTempEcharts();
     });
+    $scope.changeTempEcharts=function () {
+        $http.get(ER.coldroot+'/i/coldStorageSet/findStorageSetByUserId' ,{params: {rdcId:$rootScope.rdcId,userId:user.id,type:user.type}} ).success(function(data,status,headers,config){// 初始化冷库
+            $rootScope.Tempset=[];
+            $rootScope.mystorages = data;
+            $rootScope.storageModal = data[0];
+            $scope.initData();
+        });
+    }
 
     function findByRdcId(rootRdcId) {
         $http.get(ER.coldroot + '/i/rdc/findRDCByRDCId?rdcID=' + rootRdcId).success(function (data) {
@@ -70,6 +116,63 @@ app.controller('alarmLog', function ($scope, $location, $http, $rootScope, userS
                 }
             });
         }
+    };
+    $scope.dwechar=function(xAxis,count,time){
+
+        // 指定图表的配置项和数据
+        var option = {
+            legend: { data:['告警次数','告警时长'] },
+            tooltip: {  trigger: 'axis', axisPointer: { type: 'cross', crossStyle: { color: '#999'  }}  },
+            xAxis: [
+                {
+                    type: 'category',
+                    axisPointer: {  type: 'shadow' },
+                    data: xAxis
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value',
+                    name: '次数/次',
+                    min: 0,max: 250,interval: 50,
+                    axisLabel: { formatter: '{value}' }
+                },
+                {
+                    type: 'value',
+                    name: '时长/min',
+                    min: 0,
+                    max: 25,
+                    interval: 5,
+                    axisLabel: {
+                        formatter: '{value}'
+                    }
+                }
+            ],
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [
+                {
+                    name:'告警次数',
+                    type:'bar',
+                    data:count
+                },
+                {
+                    name:'告警时长',
+                    type:'line',
+                    yAxisIndex: 1,
+                    data:time
+                }
+            ]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+
+
     }
     $scope.changeRdc = function (rdc) {
         $scope.rdcId = rdc.id;
@@ -78,5 +181,6 @@ app.controller('alarmLog', function ($scope, $location, $http, $rootScope, userS
         $scope.alarmTotalCnt = 0;
         $scope.viewStorage(rdc.id);
         initAllByRdcId(rdc.id);
+        $scope.changeTempEcharts();
     }
 });
