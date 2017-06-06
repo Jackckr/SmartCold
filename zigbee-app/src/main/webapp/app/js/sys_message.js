82b6fdccd60071a5ecddccc8291e8d187dc26289
jhy <wellsea81>
2017-06-06 17:44:41 +0800

    pc 消息通知style

--- zigbee-app/src/main/webapp/app/js/sys_message.js
+++ zigbee-app/src/main/webapp/app/js/sys_message.js
@@ -5,7 +5,9 @@ angular.module('message', []).controller('message', function($scope,$http, $time
     $http.defaults.withCredentials=true;
     $http.defaults.headers={'Content-Type': 'application/x-www-form-urlencoded'};
 	$.ajax({ url: ER.root+"/i/UtilController/setVisited",type: "POST",data:{type:6}});
-
-
+    $scope.tabNum=1;
+    $scope.tab = function (num) {
+        $scope.tabNum = num;
+    }
 
 });
