339ecd738f45004de6e4b319d0fac52ee4c3be1e
jhy <wellsea81>
2017-06-29 15:31:30 +0800

    zigbee-web-new 冷库详情生成二维码

--- zigbee-web/src/main/webapp/view/js/rdcinfo.js
+++ zigbee-web/src/main/webapp/view/js/rdcinfo.js
@@ -1,1 +1,18 @@
+/*缩略图轮播*/
 foucsbox(2500);
+/*生成二维码*/
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+    width : 88,
+    height : 88
+});
+
+function makeCode () {
+    var elText = document.URL;
+    if (!elText) {
+        alert("二维码生成失败……");
+        return;
+    }
+    qrcode.makeCode(elText);
+}
+makeCode ();
+/*生成二维码 end*/
