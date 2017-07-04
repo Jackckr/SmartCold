b4327ea17a8f168ab46a388f3c106609167c71bc
dongping.yu <857885610@qq.com>
2017-07-04 12:27:47 +0800

    首页新版改版

--- zigbee-app-new/src/main/webapp/app/js/coldStorageAdd.js
+++ zigbee-app-new/src/main/webapp/app/js/coldStorageAdd.js
@@ -358,7 +358,7 @@ angular.module('rdcadd', ['remoteValidation','ngFileUpload']).controller('coldSt
                 data["honor" + j] = $scope.totalhonorfiles[j];
             }
             data["standPic"]=$scope.tempStandPic[0];
-            data["auditPic"]=$scope.auditPic[0]
+            data["auditPic"]=$scope.auditPic[0];
             Upload.upload({
                 url: ER.root+'/i/rdc/newAddRdc',
                 headers :{ 'Content-Transfer-Encoding': 'utf-8' },
