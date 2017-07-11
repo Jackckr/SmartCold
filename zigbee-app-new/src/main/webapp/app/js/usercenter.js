c56a2f43c7fd473af56d673cfd936218d8cb4631
maqiang34 <806487943@qq.com>
2017-07-11 10:35:45 +0800

    merge

--- zigbee-app-new/src/main/webapp/app/js/usercenter.js
+++ zigbee-app-new/src/main/webapp/app/js/usercenter.js
@@ -1,5 +1,5 @@
 var app = angular.module('app', []);
-ngApp.config(function ($httpProvider) {
+app.config(function ($httpProvider) {
     if (!$httpProvider.defaults.headers.get) { $httpProvider.defaults.headers.get = {};}    // Initialize get if not there
     $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
     $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';  //禁用IE对ajax的缓存
