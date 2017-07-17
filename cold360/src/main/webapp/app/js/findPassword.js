54389929b74a9e330b5e1250a3952c8c391881c1
jhy <wellsea81>
2017-07-17 10:46:20 +0800

    h5+h5360 注册登录忘记密码修改

--- cold360/src/main/webapp/app/js/findPassword.js
+++ cold360/src/main/webapp/app/js/findPassword.js
@@ -1,112 +1,102 @@
-var app = angular.module('app', []).controller('findPassword',function($http, $location, $scope) {
-	$http.defaults.withCredentials = true;$http.defaults.headers = {'Content-Type' : 'application/x-www-form-urlencoded'};
-	$scope.vsphone = function(telephone) {// 验证手机号码
-		var length = (telephone + '').length;
-		var mobile = /^1[3|4|5|7|8][0-9]\d{4,8}$/;
-		return telephone && length == 11&& mobile.test(telephone);
-	};
-	$scope.vertelephone = function() {// 验证手机号码
-		var ct = $scope.vsphone($scope.telephone);
-		if(ct){
-				$.post( ER.root+"/i/user/existenceUserName", {userName: $scope.telephone}, function(data) {
-	                if(data){
-	                	$("#mention1").html("");
-	                	$("#but_vercode").attr("disabled", !ct).css("background-color", ct ? "#438BCB" : "#cccccc");
-					}else{
-						$("#mention1").html("系统未找到该用户信息！");
-					}
-				});//
-		}else{
-			$("#mention1").html("");
-			$("#but_vercode").attr("disabled", true).css("background-color", "#cccccc");
-		}
-	};
-	
-	$scope.getVerCode = function() {
-		setTime(document.getElementById("but_vercode"));
-		$scope.getMobileCode('user_findwpd', $scope.telephone,'#but_vercode');
-	};
-	$scope.getMobileCode = function(key, telephone, vcid) {//获取验证码
-		$http.get(ER.root+ "/i/ShareRdcController/sharvistPhone.json",{params : {key : 'user_findwpd',telephone : telephone}}).success(function(data) {
-				if (data.success) {$scope.mtvarcode = data.entity;$(vcid).data('vc', true);}
-				//alert(data.message);
-				layer.open({
-    		 	    content: data.message
-    		 	    ,btn: '确定'
-    		 	  });
-		});
-	};
-    $scope.veteleCode = function() {// 验证码
-	 if ($("#but_vercode").data('vc') == true) {
-		var ct = $scope.verrcode&& $scope.mtvarcode&& ($scope.mtvarcode.toLowerCase() == $scope.verrcode.toLowerCase());
-		$("#app_but1").attr("disabled", !ct);
-		if (ct) {$("#app_but1").removeClass("gray");} else {$("#app_but1").addClass("gray");}
-	} 
-//	 else {
-//		$http.get(ER.root+ "/i/ShareRdcController/sharvistCode",{params : {key : 'user_findwpd',telephone :$scope.telephone,yzm : $scope.verrcode}}).success(function(data) {
-//			if (data) {
-//				$("#app_but1").attr("disabled", false);
-//				$("#app_but1").removeClass("gray");
-//			} else {
-//				$("#app_but1").attr("disabled", true);$("#app_but1").addClass("gray");
-//			}
-//		});
-//	}
-   };
-   $scope.checkData=function (){ 
-  	      var password = $("#txt_password").val().trim(); 
-          var repsword = $("#txt_repsword").val().trim(); 
-           if (password.length!=0 && repsword.length!=0) {
-        	   if(password.length<6){
-        		   $("#mention2").html("密码长度不能小于6位！");
-        		   return false;
-        	   }else if(password.length>16){
-        		   $("#mention2").html("密码长度最多16位！");
-       		       return false;
-        	   }
-           	   if(password != repsword) {
-		         $("#mention2").html("两次密码输入不一致，请重新输入");
-		         return false;
-		       }
-		       else {
-		    		$scope.savedata();
-		       }
-           } else{
-        	/* if(password.length==0){
-        		 $("#txt_password").focus();
-        	 }else if(repsword.length==0){
-        		 $("#txt_repsword").focus();
-        	 }*/
-           	  $("#mention2").html("密码不能为空哦，请输入密码~");
-           }
-  	  };
-   	$scope.savedata= function () {// 修改密码
-		var me = "#btn_login"; if ($(me).data('isLoading') === true) return;$(me).text("提交中...");$("#mention2").html(""); //防止再次点击
-        $.ajax({
-        	type: 'POST',
-        	data: {key:'user_findwpd',username:$scope.telephone,toke:$scope.verrcode,password:$("#txt_repsword").val().trim()},
-            url: ER.root+"/i/user/upPwdByTelephone",
-            complete : function(e){$(me).text("确定"); $(me).delay(500).data('isLoading',false);},
-            success: function(data){
-            	if(data.success){
-            		//alert("密码重置成功！");
-            		layer.open({
-	    		 	    content: '密码重置成功咯~'
-	    		 	    ,btn: '确定'
-	    		 	  });
-            		window.location.href = "../index.html";
-            	}else{
-            		$("#mention2").html("修改失败~请稍后重试~");
-            	} }
-          });
-	};
-	$scope.initevg = function() {
-		$("#app_but1").click(function() {
-			$("#stup1").hide();
-			$("#stup2").show();
-			$("#title_div").html("修改密码");
-		});
-	};
-	$scope.initevg();
-	var tel = util.getCookie("username");if ($scope.vsphone(tel)) {$scope.telephone=tel; $("#but_vercode").attr("disabled", false).css("background-color", "#438BCB" );}
+var app = angular.module('app', []).controller('findPassword', function ($http, $location, $scope) {
+    $http.defaults.withCredentials = true;
+    $http.defaults.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+    $scope.vsphone = function (telephone) {// 验证手机号码
+        var length = (telephone + '').length;
+        var mobile = /^1[3|4|5|7|8][0-9]\d{4,8}$/;
+        return telephone && length == 11 && mobile.test(telephone);
+    };
+    localStorage.goIndex = 1;
+    var phoneReg=/^(((13[0-9]{1})|(14[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})$/,vifcode=null;
+    $scope.vertelephone = function () {// 验证手机号码
+        var ct = $scope.vsphone($scope.telephone);
+        if (ct) {
+            $("#mention1").html("");
+        } else {
+            $("#mention1").html("手机号格式有误~~");
+        }
+    };
+
+    $scope.getVerCode = function () {
+        if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test($scope.username)){
+            layer.open({content: "用户名不能为空并且不能有特殊字符", btn: '确定'});
+            return false
+        }else if(!/^[a-zA-Z0-9][a-zA-Z0-9]{2,23}$/.test($scope.username)){
+            layer.open({content: "用户名不能包含中文和特殊字符，长度不得低于3位~", btn: '确定'});
+            return false
+        }else{
+            if(phoneReg.test($scope.telephone)){
+                $('#telNum').attr('readonly','readonly');
+                $('#but_vercode').attr('disabled','disabled').css('backgroundColor','#cccccc');
+                $.post(ER.root+'/i/user/telephoneSIDVerify',{
+                    telephone:$('#telNum').val().trim()
+                }, function(data){
+                    vifcode=data;
+                    layer.open({content: data.message, btn: '确定'});
+                });
+            }else{
+                layer.open({content: "请输入正确的手机号", btn: '确定'});
+            }
+        }
+    };
+    $scope.checkData = function ($event) {
+        var password = $scope.password;
+        var repsword = $scope.password1;
+        if (password.length == "") {
+            $("#mention1").html("密码不能为空~");
+            return false;
+        } else if (/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(password)) {
+            if (password != repsword) {
+                $("#mention1").html("两次密码输入不一致，请重新输入");
+                return false;
+            } else {
+                $("#mention1").html("");1
+            }
+        } else {
+            $("#mention1").html("密码长度6-12位,必须是数字字母组合");
+        }
+    };
+    $scope.savedata = function () {// 修改密码
+        if($scope.username==undefined||$scope.telephone==undefined||
+            $scope.verrcode==undefined||$scope.password==undefined||
+            $scope.password1==undefined){
+            layer.open({content: "请完善信息再提交~", btn: '确定'});
+            return false
+        }
+        if($scope.password===$scope.password1){
+            if(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test($scope.password1)&&/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test($scope.password)){
+                $("#mention2").html(""); //防止再次点击
+                $.ajax({
+                    type: 'POST',
+                    data: {
+                        username: $scope.username,
+                        telephone: $scope.telephone,
+                        toke: $scope.verrcode,
+                        password: $scope.password,
+                        password1: $scope.password1,
+                        stoken: vifcode.entity
+                    },
+                    url: ER.root + "/i/user/resetPwdByUserName",
+                    success: function (data) {
+                        if (data.success) {
+                            layer.open({
+                                content: data.message,
+                                btn: '确定',
+                                yes: function(){
+                                    window.location.href = "../index.html";
+                                }
+                            });
+                        } else {
+                            layer.open({content: data.message, btn: '确定'});
+                            $("#mention2").html("修改失败~请稍后重试~");
+                        }
+                    }
+                });
+            }else{
+                layer.open({content: "密码长度6-12位,必须是数字字母组合", btn: '确定'});
+            }
+        }else{
+            layer.open({content: "两次密码输入不一致，请重新输入", btn: '确定'});
+        }
+    };
 });
--- cold360/src/main/webapp/app/js/register.js
+++ cold360/src/main/webapp/app/js/register.js
@@ -1,109 +1,145 @@
-var app = angular.module('app', []).controller('register',function($http, $location, $scope) {
-	$http.defaults.withCredentials = true;$http.defaults.headers = {'Content-Type' : 'application/x-www-form-urlencoded'};
-	var victdata={victtl:false,extname:false,victyzm:false,victpwd:false,tel:null};
-	$scope.vsphone = function(telephone) {// 验证手机号码
-		var length = (telephone + '').length;
-//		var mobile = /^1[3|4|5|7|8][0-9]\d{4,8}$/;
-		var mobile = /^(13[0-9]|14[0-9]|15[0-9]|17[0-9]|18[0-9])\d{8}$/;
-		return telephone && length == 11&& mobile.test(telephone);
-	};
-	$scope.vertelephone = function() {// 验证手机号码
-		$("#code2").val('');$("#but_vercode").data('vc', false);_sysconfig.countdown =0;
-		var ct = $scope.vsphone($scope.telephone);
-		if(ct){
-			victdata.victtl=true;
-			$.post( ER.root+"/i/user/existenceUserName", {userName: $scope.telephone}, function(data) {
-                victdata.extname=!data;
-                $("#mention1").html(data?"该手机已经注册~":"");
-                $("#but_vercode").attr("disabled", data).css("background-color", data ?  "#cccccc":"#438BCB" );
-			});//
-		}else{
-			 victdata.victtl=false;
-			 $("#mention1").html("请输入正确手机号码~");
-			 $("#but_vercode").attr("disabled", true).css("background-color",  "#cccccc");
-		}
-	};
-	
-	$scope.getVerCode = function() {
-		setTime(document.getElementById("but_vercode"));
-		$scope.getMobileCode('user_register', $scope.telephone,'#but_vercode');
-	};
-	$scope.getMobileCode = function(key, telephone, vcid) {//获取验证码
-		$http.get(ER.root+ "/i/ShareRdcController/sharvistPhone.json",{params : {key : 'signUpCode',telephone : telephone}}).success(function(data) {
-				if (data.success) {$scope.mtvarcode = data.entity;$(vcid).data('vc', true);}
-				//alert(data.message);
-				layer.open({
-				    content: data.message
-				    ,btn: '确定'
-				  });
-		});
-	};
+var app=angular.module('app', []).controller('register',function($http, $location, $scope) {
+    $http.defaults.withCredentials = true;$http.defaults.headers = {'Content-Type' : 'application/x-www-form-urlencoded'};
+    var victdata={victtl:false,extname:false,victyzm:false,victpwd:false,tel:null,usefulName:false};
+    $scope.vsphone = function(telephone) {// 验证手机号码
+        var length = (telephone + '').length;
+        var mobile = /^(13[0-9]|14[0-9]|15[0-9]|17[0-9]|18[0-9])\d{8}$/;
+        return telephone && length == 11&& mobile.test(telephone);
+    };
+    $("#username").blur(function () {
+        $.post(ER.root + '/i/user/userNameVerify',{
+            username:$("#username").val()
+        }, function(data){
+            if(!data){
+                layer.open({
+                    content: "用户名已存在,请更换~"
+                    ,btn: '确定'
+                });
+                victdata.extname=false;
+                return false
+            }else{
+                if(!/^[a-zA-Z0-9][a-zA-Z0-9]{2,23}$/.test($("#username").val())){
+                    layer.open({
+                        content: "用户名不能包含中文和特殊字符，长度3~24位~"
+                        ,btn: '确定'
+                    });
+                    victdata.usefulName=true;
+                    return false
+                }else{
+                    victdata.extname=true;
+                }
+            }
+        });
+    });
+    $scope.vertelephone = function() {// 验证手机号码
+        $("#code2").val('');$("#but_vercode").data('vc', false);
+        var ct = $scope.vsphone($scope.telephone);
+        if(ct){
+            victdata.victtl=true;
+            $("#mention1").html('');
+            $("#but_vercode").attr("disabled", false).css("background-color",  "#4287ff");
+        }else{
+            victdata.victtl=false;
+            $("#mention1").html("请输入正确手机号码~");
+            $("#but_vercode").attr("disabled", true).css("background-color",  "#cccccc");
+        }
+    };
+
+    $scope.getVerCode = function() {
+        if(victdata.extname){//发验证码之前先检查用户名是否可用
+            $scope.getMobileCode('user_register', $scope.telephone,'#but_vercode');
+            $("#but_vercode").attr("disabled", true).css("background-color",  "#cccccc");
+            $("#telNum").attr('readonly','readonly');
+            $("#username").attr('readonly','readonly');
+        }else{
+            if(victdata.usefulName){
+                layer.open({
+                    content: "用户名不能包含中文和特殊字符，长度不得低于3位~"
+                    ,btn: '确定'
+                });
+            }else{
+                layer.open({
+                    content: "用户名已存在,请更换~"
+                    ,btn: '确定'
+                });
+            }
+        }
+
+    };
+    $scope.getMobileCode = function(key, telephone, vcid) {//获取验证码
+        $http.get(ER.root+ "/i/ShareRdcController/sharvistPhone.json",{params : {key : 'signUpCode',telephone : telephone}}).success(function(data) {
+            if (data.success) {$scope.mtvarcode = data.entity;$(vcid).data('vc', true);}
+            layer.open({
+                content: data.message
+                ,btn: '确定'
+            });
+        });
+    };
     $scope.veteleCode = function() {// 验证码
-    	if ($("#but_vercode").data('vc') == true) {
-    		victdata.victyzm= $scope.verrcode&& $scope.mtvarcode&& ($scope.mtvarcode.toLowerCase() == $scope.verrcode.toLowerCase());
-    	} 
-   };
-   $scope.checkpwd=function(){
-	   var password = $("#txt_password").val().trim(); 
-       var repsword = $("#txt_repsword").val().trim();
-       if(password == ''){
-           $("#mention1").html("密码不能为空");
-           $('#app_but1').attr("disabled",true);
-           return false;
-       }else if(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(password)){
-           if(password != repsword){
-               $("#mention1").html("两次密码输入不一致，请重新输入");
-               $('#app_but1').attr("disabled",true);
-               return false;
-           }else{
-               $("#mention1").html("");
-               $('#app_but1').attr("disabled",false);
-           }
-       }else{
-           $("#mention1").html("密码长度6-12位,必须是数字字母组合");
-           $('#app_but1').attr("disabled",true);
-       }
-   }
-   $scope.checkData=function (){ 
-	      $("#mention1").html();
-	      if(!victdata.victtl){ $("#mention1").html("请输入正确手机号码~");return false; }
-	      if(!victdata.extname){ $("#mention1").html("该手机已经注册~");return false; }
-	      if(!victdata.victyzm){ $("#mention1").html("验证码错误~请重新输入~");return false; }
-	      $scope.savedata();
-  	      
-  	  };
+        if ($("#but_vercode").data('vc') == true) {
+            victdata.victyzm= $scope.verrcode&& $scope.mtvarcode&& ($scope.mtvarcode.toLowerCase() == $scope.verrcode.toLowerCase());
+        }
+    };
+    $scope.checkpwd=function(){
+        var password = $("#txt_password").val().trim();
+        var repsword = $("#txt_repsword").val().trim();
+
+        if(password == ''){
+            $("#mention1").html("密码不能为空");
+            $('#app_but1').attr("disabled",true);
+            return false;
+        }else if(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{5,15}$/.test(password)){
+            if(password != repsword){
+                $("#mention1").html("两次密码输入不一致，请重新输入");
+                $('#app_but1').attr("disabled",true);
+                return false;
+            }else{
+                $("#mention1").html("");
+                $('#app_but1').attr("disabled",false);
+            }
+        }else{
+            $("#mention1").html("密码长度6-16位,必须是数字字母组合");
+            $('#app_but1').attr("disabled",true);
+        }
+    }
+    $scope.checkData=function (){
+        $("#mention1").html();
+        if(!victdata.victtl){ $("#mention1").html("请输入正确手机号码~");return false; }
+        if(!victdata.victyzm){ $("#mention1").html("验证码错误~请重新输入~");return false; }
+        $scope.savedata();
+
+    };
     $scope.ischeck=true;
-   	$scope.savedata= function () {// 修改密码
-        if($scope.ischeck==false){
+    $scope.savedata = function () {// 修改密码
+        if ($scope.ischeck == false) {
             alert("请确认您已同意冷库360使用协议~");
             return
         }
-		var me = "#btn_login"; if ($(me).data('isLoading') === true) return;$(me).text("提交中...");$("#mention2").html(""); //防止再次点击
-        $('#app_but1').attr("disabled",true);
-		$.ajax({
-        	type: 'POST',
-        	data:{username:$("#telNum").val().trim(),
-				password:$("#txt_password").val().trim(),
-				password1:$("#txt_repsword").val().trim(),
-				telephone:$("#telNum").val().trim(),
-				type:$("input[name='usertype']:checked").val(),
-				signUpCode:$("#code2").val().trim()
-		},
-            url: ER.root+"/i/user/signup?",
-            complete : function(e){$(me).text("注册"); $(me).delay(500).data('isLoading',false);},
-            success: function(data){
-            	if(data.status=="0"){
-            		//alert(data.message);
-            		layer.open({
-    				    content: data.message
-    				    ,btn: '确定'
-    				  });
-            		window.location.href = "../index.html";
-            	}else{
-            		$("#mention1").html(data.message);
-            	} }
-          });
-        
-        
-	};
+        $("#mention2").html(""); //防止再次点击
+        $('#app_but1').attr("disabled", true);
+        $.ajax({
+            type: 'POST',
+            data: {
+                username: $("#username").val().trim(),
+                password: $("#txt_password").val().trim(),
+                password1: $("#txt_repsword").val().trim(),
+                telephone: $("#telNum").val().trim(),
+                signUpCode: $("#code2").val().trim()
+            },
+            url: ER.root + "/i/user/signup?",
+            success: function (data) {
+                if (data.status == "0") {
+                    layer.open({
+                        content: data.message
+                        , btn: '确定'
+                        , yes: function () {
+                            window.location.href = "../index.html";
+                        }
+                    });
+                } else {
+                    $("#mention1").html(data.message);
+                }
+            }
+        });
+    };
 });
