ece79dfdee4dcd129689db5397d7977b8ab74ed4
dongping.yu <857885610@qq.com>
2017-08-15 16:45:36 +0800

    首页新版改版

--- bgzigbee-web/src/main/java/com/smartcold/bgzigbee/manage/sc360/entity/Company.java
+++ bgzigbee-web/src/main/java/com/smartcold/bgzigbee/manage/sc360/entity/Company.java
@@ -11,6 +11,12 @@ public class Company {
 
     private Date addtime;
 
+    private String email;
+
+    private String principal;
+
+    private String telephone;
+
     public Integer getId() {
         return id;
     }
@@ -41,5 +47,29 @@ public class Company {
 
     public void setAddtime(Date addtime) {
         this.addtime = addtime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(String principal) {
+        this.principal = principal;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
