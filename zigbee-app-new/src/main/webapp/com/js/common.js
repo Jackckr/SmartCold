abe0b1a3633d492f0ac529ddb26cdd309aad32b4
jhy <wellsea81>
2017-08-21 17:50:59 +0800

    pc链库再注册验证码更改

--- zigbee-app-new/src/main/webapp/com/js/common.js
+++ zigbee-app-new/src/main/webapp/com/js/common.js
@@ -28,8 +28,8 @@ var oHtml = document.documentElement;
 var _sysconfig={countdown:60,isdebug:true,resize:true};
 var screenWidth = oHtml.clientWidth,screenHeight = oHtml.clientHeight;
 getFont();$(window).resize(function(event) { if(_sysconfig.resize)getFont();});
-  var ER = {root:"http://www.liankur.com/",coldroot:"http://www.smartcold.org.cn"};
-// var ER = {root:"http://192.168.1.114:8080",coldroot:"http://www.smartcold.org.cn"};
+  // var ER = {root:"http://www.liankur.com/",coldroot:"http://www.smartcold.org.cn"};
+var ER = {root:"http://192.168.1.114:8080",coldroot:"http://www.smartcold.org.cn"};
 if ($.ajax) {jQuery.ajaxSetup({cache:false,xhrFields:{withCredentials:true}});}//支持ajax跨域
 if(localStorage.length>=14){for(var i in localStorage ){if(i.indexOf("BMap_")>=0){ localStorage.removeItem(i);}}}
 if(window.user==undefined ||window.user==null){
