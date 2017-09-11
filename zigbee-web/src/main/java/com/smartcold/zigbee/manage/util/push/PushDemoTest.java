610c0b558dea440428673253a276946d78d423d7
dongping.yu <857885610@qq.com>
2017-09-11 18:18:07 +0800

    推送环境

---
+++ zigbee-web/src/main/java/com/smartcold/zigbee/manage/util/push/PushDemoTest.java
@@ -0,0 +1,152 @@
+package com.smartcold.zigbee.manage.util.push;
+
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.http.ProtocolType;
+import com.aliyuncs.push.model.v20150827.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+/**
+ * 推送的OpenAPI文档 https://help.aliyun.com/document_detail/30074.html
+ */
+public class PushDemoTest extends BaseTest {
+    /**
+     * 推送通知给android
+     * <p>
+     * 参见文档 https://help.aliyun.com/document_detail/30082.html
+     */
+    public void testPushNoticeToAndroid_toAll() throws Exception {
+        PushNoticeToAndroidRequest androidRequest = new PushNoticeToAndroidRequest();
+        //推送内容需要保护，请使用HTTPS协议
+        androidRequest.setProtocol(ProtocolType.HTTPS);
+        //推送内容较长，请使用POST请求
+        androidRequest.setMethod(MethodType.POST);
+        androidRequest.setAppKey(appKey);
+        androidRequest.setTarget("all");
+        androidRequest.setTargetValue("all");
+        androidRequest.setTitle("Hello OpenAPI!");
+        androidRequest.setSummary("你好, PushNoticeToAndroid from OpenAPI!");
+        androidRequest.setAndroidExtParameters("{\"key1\":\"value1\",\"api_name\":\"PushNoticeToAndroidRequest\"}");
+
+        PushNoticeToAndroidResponse pushNoticeToAndroidResponse = client.getAcsResponse(androidRequest);
+        System.out.printf("RequestId: %s, ResponseId: %s\n",
+                pushNoticeToAndroidResponse.getRequestId(), pushNoticeToAndroidResponse.getResponseId());
+    }
+
+    /**
+     * 推送通知给android
+     * <p>
+     * 参见文档 https://help.aliyun.com/document_detail/30082.html
+     */
+    public void testPushNoticeToAndroid_toDevice() throws Exception {
+
+        PushNoticeToAndroidRequest androidRequest = new PushNoticeToAndroidRequest();
+        //推送内容需要保护，请使用HTTPS协议
+        androidRequest.setProtocol(ProtocolType.HTTPS);
+        //推送内容较长，请使用POST请求
+        androidRequest.setMethod(MethodType.POST);
+        androidRequest.setAppKey(appKey);
+        androidRequest.setTarget("device");
+        androidRequest.setTargetValue(deviceIds);
+        androidRequest.setTitle("Hello OpenAPI!");
+        androidRequest.setSummary("你好, PushNoticeToAndroid from OpenAPI!");
+
+        PushNoticeToAndroidResponse pushNoticeToAndroidResponse = client.getAcsResponse(androidRequest);
+        System.out.printf("RequestId: %s, ResponseId: %s\n",
+                pushNoticeToAndroidResponse.getRequestId(), pushNoticeToAndroidResponse.getResponseId());
+    }
+
+    /**
+     * 推送通知给android
+     * <p>
+     * 参见文档 https://help.aliyun.com/document_detail/30082.html
+     */
+    public void testPushNoticeToAndroid_toAccount() throws Exception {
+        PushNoticeToAndroidRequest androidRequest = new PushNoticeToAndroidRequest();
+        //推送内容需要保护，请使用HTTPS协议
+        androidRequest.setProtocol(ProtocolType.HTTPS);
+        //推送内容较长，请使用POST请求
+        androidRequest.setMethod(MethodType.POST);
+        androidRequest.setAppKey(appKey);
+        androidRequest.setTarget("account");
+        androidRequest.setTargetValue(accounts);
+        androidRequest.setTitle("Hello OpenAPI!");
+        androidRequest.setSummary("你好, PushNoticeToAndroid from OpenAPI!");
+
+        PushNoticeToAndroidResponse pushNoticeToAndroidResponse = client.getAcsResponse(androidRequest);
+        System.out.printf("RequestId: %s, ResponseId: %s\n",
+                pushNoticeToAndroidResponse.getRequestId(), pushNoticeToAndroidResponse.getResponseId());
+    }
+
+    /**
+     * 推送消息给android
+     * <p>
+     * 参见文档 https://help.aliyun.com/document_detail/30081.html
+     */
+    public void testPushMessageToAndroid_toAll() throws Exception {
+        PushMessageToAndroidRequest androidRequest = new PushMessageToAndroidRequest();
+        //推送内容需要保护，请使用HTTPS协议
+        androidRequest.setProtocol(ProtocolType.HTTPS);
+        //推送内容较长，请使用POST请求
+        androidRequest.setMethod(MethodType.POST);
+        androidRequest.setAppKey(appKey);
+        androidRequest.setTarget("all");
+        androidRequest.setTargetValue("all");
+        androidRequest.setMessage("PushMessageToAndroid from OpenAPI!");
+
+        PushMessageToAndroidResponse pushMessageToAndroidResponse = client.getAcsResponse(androidRequest);
+        System.out.printf("RequestId: %s, ResponseId: %s\n",
+                pushMessageToAndroidResponse.getRequestId(), pushMessageToAndroidResponse.getResponseId());
+    }
+
+    /**
+     * 推送通知给iOS
+     * <p>
+     * 参见文档 https://help.aliyun.com/document_detail/30084.html
+     */
+    public void testPushNoticeToIOS_toAll(String target,String targetValue,String summary) throws Exception {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd HH:mm:ss");
+
+        PushNoticeToiOSRequest iOSRequest = new PushNoticeToiOSRequest();
+        //推送内容需要保护，请使用HTTPS协议
+        iOSRequest.setProtocol(ProtocolType.HTTPS);
+        //推送内容较长，请使用POST请求
+        iOSRequest.setMethod(MethodType.POST);
+        iOSRequest.setAppKey(appKey);
+        // iOS的通知是通过APNS中心来发送的，需要填写对应的环境信息. DEV:表示开发环境, PRODUCT: 表示生产环境
+        iOSRequest.setEnv("DEV");
+        iOSRequest.setTarget(target);
+        iOSRequest.setTargetValue(targetValue);
+        iOSRequest.setSummary(summary);
+        iOSRequest.setiOSExtParameters("{\"k1\":\"v1\",\"k2\":\"v2\"}");
+        iOSRequest.setExt("{\"sound\":\"default\", \"badge\":\"42\"}");
+
+        PushNoticeToiOSResponse pushNoticeToiOSResponse = client.getAcsResponse(iOSRequest);
+        System.out.printf("RequestId: %s, ResponseId: %s\n",
+                pushNoticeToiOSResponse.getRequestId(), pushNoticeToiOSResponse.getResponseId());
+    }
+
+    /**
+     * 推送消息给iOS
+     * <p>
+     * 参见文档 https://help.aliyun.com/document_detail/30083.html
+     */
+    public void testPushMessageToIOS_toAll() throws Exception {
+        PushMessageToiOSRequest iOSRequest = new PushMessageToiOSRequest();
+        //推送内容需要保护，请使用HTTPS协议
+        iOSRequest.setProtocol(ProtocolType.HTTPS);
+        //推送内容较长，请使用POST请求
+        iOSRequest.setMethod(MethodType.POST);
+        iOSRequest.setAppKey(appKey);
+        iOSRequest.setTarget("all");
+        iOSRequest.setTargetValue("all");
+        iOSRequest.setMessage("message");
+        iOSRequest.setSummary("summary");
+
+        PushMessageToiOSResponse pushMessageToiOSResponse = client.getAcsResponse(iOSRequest);
+        System.out.printf("RequestId: %s, ResponseId: %s\n",
+                pushMessageToiOSResponse.getRequestId(), pushMessageToiOSResponse.getResponseId());
+    }
+}
---
+++ zigbee-web/src/main/java/com/smartcold/zigbee/manage/util/push/TagDemoTest.java
@@ -0,0 +1,85 @@
+package com.smartcold.zigbee.manage.util.push;
+
+import com.aliyuncs.push.model.v20150827.*;
+import java.util.List;
+
+/**
+ * 标签操作Demo
+ */
+public class TagDemoTest extends BaseTest {
+
+    /**
+     * 查询App所有的标签列表
+     * 参考文档 ：https://help.aliyun.com/document_detail/30089.html
+     * */
+    public void testListTags() throws Exception {
+
+        ListTagsRequest request = new ListTagsRequest();
+        request.setAppKey(appKey);
+
+        ListTagsResponse response = client.getAcsResponse(request);
+        System.out.printf("RequestId : %s\n" , response.getRequestId());
+
+        List<ListTagsResponse.TagInfo> tagInfos = response.getTagInfos();
+        for (ListTagsResponse.TagInfo tagInfo : tagInfos) {
+            System.out.printf("tagName : %s\n", tagInfo.getTagName());
+        }
+
+    }
+
+    /**
+     * 查询某个设备的标签列表
+     * 参考文档 ： https://help.aliyun.com/document_detail/30090.html
+     * */
+    public void testQueryTag() throws Exception {
+
+        QueryTagsRequest request = new QueryTagsRequest();
+        request.setAppKey(appKey);
+        request.setClientKey(deviceIds);
+        request.setKeyType(1);//1 : device 2 : account 3 : alias
+
+        QueryTagsResponse response = client.getAcsResponse(request);
+        System.out.printf("RequestId : %s\n " , response.getRequestId());
+
+        List<QueryTagsResponse.TagInfo> tagInfos = response.getTagInfos();
+        for (QueryTagsResponse.TagInfo tagInfo : tagInfos) {
+            System.out.printf("tagName : %s\n", tagInfo.getTagName());
+        }
+
+    }
+
+    /**
+     * 绑定标签
+     * 参考文档 ：https://help.aliyun.com/document_detail/30087.html
+     * */
+    public void testBindTag() throws Exception {
+
+        BindTagRequest request = new BindTagRequest();
+        request.setAppKey(appKey);
+        request.setClientKey(deviceIds);
+        request.setKeyType(1);//1 : device 2 : account 3 : alias
+        request.setTagName("tag1");
+
+        BindTagResponse response = client.getAcsResponse(request);
+        System.out.printf("RequestId : %s\n " , response.getRequestId());
+
+    }
+
+    /**
+     * 解绑标签
+     * 参考文档 ：https://help.aliyun.com/document_detail/30088.html
+     * */
+    public void testUnbindTag() throws Exception {
+
+        UnbindTagRequest request = new UnbindTagRequest();
+        request.setAppKey(appKey);
+        request.setClientKey(deviceIds);
+        request.setKeyType(1);//1 : device 2 : account 3 : alias
+        request.setTagName("tag1");
+
+        UnbindTagResponse response = client.getAcsResponse(request);
+        System.out.printf("RequestId : %s\n " , response.getRequestId());
+
+    }
+
+}
