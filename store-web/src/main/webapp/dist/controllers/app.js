139358dce584bf5f4385e9865112d8fbfd38f3cc
jhy <857885610@qq.com>
2017-09-14 17:54:14 +0800

    杂项修改

--- store-web/src/main/webapp/dist/controllers/app.js
+++ store-web/src/main/webapp/dist/controllers/app.js
@@ -2,7 +2,21 @@
 * store 360 system
 * date:2017-09-13
 * */
-var storeWeb = angular.module('storeWeb', ['ui.bootstrap', 'ui.router']);
+var storeWeb = angular.module('storeWeb', ['ui.bootstrap', 'ui.router', 'ngCookies']);
+angular.element(document).ready(function() {
+    $.ajax({
+        url: '',
+        type: 'GET',
+        dataType: 'json',
+        cache: false,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+    }).done(function(data) {
+        user = data;
+        if(user.username == null){document.location.href = "/login.html";return; }
+        angular.bootstrap(document, ['storeWeb']);
+    });
+});
+
 
 storeWeb.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/blank");
---
+++ store-web/src/main/webapp/dist/controllers/login.js
@@ -0,0 +1,5 @@
+/*
+*Created by wellsea
+*Date: 2017/9/14 0014
+*Time: 13:52
+*/
