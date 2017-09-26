417a01e0055e5091203dd6cad91855ea931229e2
jhy <857885610@qq.com>
2017-09-26 11:26:35 +0800

    链库招标采购新加一个

---
+++ zigbee-web/src/main/webapp/view/js/bid.js
@@ -0,0 +1,8 @@
+/*招标采购*/
+$(function () {
+    localStorage.OURL=document.URL;
+    if(window.lkuser){
+        $('.bidTel span').html('0571-56230712');
+        $('.bidEm span').html('wulili@masterkong.com.cn');
+    }
+});
